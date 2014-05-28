3601b05bf664846e3464d34f5dffd4febdc9151c
Stefan Jakob <stevi872@freenet.de>
2014-05-28 13:40:21 +0200

    bla

---
+++ include/engine/model/SyncTransition.h
@@ -0,0 +1,28 @@
+/*
+ * SyncTransition.h
+ *
+ *  Created on: Mar 8, 2014
+ *      Author: Stephan Opfer
+ */
+
+#ifndef SYNCTRANSITION_H_
+#define SYNCTRANSITION_H_
+
+#include <AlicaElement.h>
+
+namespace alica
+{
+
+/*
+ *
+ */
+class SyncTransition : public alica::AlicaElement
+{
+public:
+	SyncTransition();
+	virtual ~SyncTransition();
+};
+
+} /* namespace Alica */
+
+#endif /* SYNCTRANSITION_H_ */
