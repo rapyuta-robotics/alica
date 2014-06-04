a49d19131ce196e534d58360bffad85fea125bf3
Stefan Jakob <stevi872@freenet.de>
2014-06-04 16:03:56 +0200

    working on behaviourconfiguration

--- include/engine/model/BehaviourConfiguration.h
+++ include/engine/model/BehaviourConfiguration.h
@@ -8,16 +8,42 @@
 #ifndef BEHAVIOURCONFIGURATION_H_
 #define BEHAVIOURCONFIGURATION_H_
 
+using namespace std;
+
+#include <map>
+#include <string>
+
 #include "AbstractPlan.h"
+#include "Behaviour.h"
 
 namespace alica
 {
-
+	class Behaviour;
 	class BehaviourConfiguration : public AbstractPlan
 	{
 	public:
 		BehaviourConfiguration();
+		BehaviourConfiguration(long id);
 		virtual ~BehaviourConfiguration();
+
+		string toString();
+
+		int getDeferring() const;
+		void setDeferring(int deferring);bool isEventDriven() const;
+		void setEventDriven(bool eventDriven);
+		int getFrequency() const;
+		void setFrequency(int frequency);
+		const map<string, string>& getParameters() const;
+		void setParameters(const map<string, string>& parameters);
+		const Behaviour* getBehaviour() const;
+		void setBehaviour(const Behaviour* behaviour);
+
+	private:
+		bool eventDriven;
+		int frequency;
+		int deferring;
+		map<string,string> parameters;
+		const Behaviour* behaviour;
 	};
 
 } /* namespace Alica */
--- src/engine/model/BehaviourConfiguration.cpp
+++ src/engine/model/BehaviourConfiguration.cpp
@@ -12,13 +12,88 @@ namespace alica
 
 	BehaviourConfiguration::BehaviourConfiguration()
 	{
-		// TODO Auto-generated constructor stub
+		this->eventDriven = false;
+		this->frequency = 30;
+		this->deferring = 0;
+		this->parameters = (*new map<string,string>);
 
+	}
+
+	BehaviourConfiguration::BehaviourConfiguration(long id) : BehaviourConfiguration()
+	{
+		this->id = id;
 	}
 
 	BehaviourConfiguration::~BehaviourConfiguration()
 	{
-		// TODO Auto-generated destructor stub
+
+	}
+
+	string BehaviourConfiguration::toString()
+	{
+		stringstream ss;
+		ss << "#BehaviourConfiguration: " << this->getName()  << " " << this->getId() << endl;
+		ss << "\tBehaviour: ";
+		if(this->getBehaviour() != NULL) {
+			ss << name << " " << this->getBehaviour()->getId();
+		}
+		ss << endl;"\tDeferring: " + this->getDeferring();
+
+		//TODO finish to string
+		return ss.str();
+	}
+
+//================== Getter and Setter =========================
+
+	int BehaviourConfiguration::getDeferring() const
+	{
+		return deferring;
+	}
+
+	void BehaviourConfiguration::setDeferring(int deferring)
+	{
+		this->deferring = deferring;
+	}
+
+	bool BehaviourConfiguration::isEventDriven() const
+	{
+		return eventDriven;
+	}
+
+	void BehaviourConfiguration::setEventDriven(bool eventDriven)
+	{
+		this->eventDriven = eventDriven;
+	}
+
+	int BehaviourConfiguration::getFrequency() const
+	{
+		return frequency;
+	}
+
+	void BehaviourConfiguration::setFrequency(int frequency)
+	{
+		this->frequency = frequency;
+	}
+
+	const map<string, string>& BehaviourConfiguration::getParameters() const
+	{
+		return parameters;
+	}
+
+	void BehaviourConfiguration::setParameters(const map<string, string>& parameters)
+	{
+		this->parameters = parameters;
+	}
+
+	const Behaviour* BehaviourConfiguration::getBehaviour() const
+	{
+		return behaviour;
+	}
+
+	void BehaviourConfiguration::setBehaviour(const Behaviour* behaviour)
+	{
+		this->behaviour = behaviour;
 	}
 
 } /* namespace Alica */
+
