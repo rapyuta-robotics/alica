b3f7a5773038cd09708c6e9801b93688b279684b
Stefan Jakob <stevi872@freenet.de>
2014-06-06 15:22:03 +0200

    working on model

--- src/engine/model/PostCondition.cpp
+++ src/engine/model/PostCondition.cpp
@@ -24,7 +24,7 @@ namespace alica
 		stringstream ss;
 		ss << "#PostCondition: " + this->name << " " << this->id << endl;
 		ss << "\t ConditionString: " << this->conditionString << endl;
-		ss << "#EndResult" << endl;
+		ss << "#PostCondition" << endl;
 		return ss.str();
 
 	}
