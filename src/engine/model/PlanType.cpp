6e42bf4c136e5850a5135ea3f232914e383bcea0
Stefan Jakob <stevi872@freenet.de>
2014-06-06 14:49:43 +0200

    working on model

--- src/engine/model/PlanType.cpp
+++ src/engine/model/PlanType.cpp
@@ -12,13 +12,69 @@ namespace alica
 
 	PlanType::PlanType()
 	{
-		// TODO Auto-generated constructor stub
 
 	}
 
 	PlanType::~PlanType()
 	{
-		// TODO Auto-generated destructor stub
 	}
 
+	string PlanType::toString()
+	{
+		stringstream ss;
+		ss << "#PlanType: " << this->name << " " << this->id << endl;
+		return ss.str();
+//		ret += "#PlanType: " + this.Name + " " + this.Id + "\n";
+//
+//		ret += "\tPlans: " +this.Plans.Count+ "\n";
+//		if(this.Plans.Count != 0)
+//		{
+//			foreach (Plan p in this.Plans)
+//			{
+//				//ret += "\t" + p;
+//				ret += "\t" + p.Id + " " + p.Name + "\n";
+//			}
+//		}
+//		ret += "#EndPlanType\n";
+//
+//		return ret;
+	}
+
+//====================== Getter and Setter =========================
+
+	const string& PlanType::getFileName() const
+	{
+		if(this->fileName.empty())
+		{
+			static string result = this->name+".pty";
+			return result;
+		}
+		else
+		{
+			return this->fileName;
+		}
+	}
+
+	const list<Parametrisation*>& PlanType::getParametrisation() const
+	{
+		return parametrisation;
+	}
+
+	void PlanType::setParametrisation(const list<Parametrisation*>& parametrisation)
+	{
+		this->parametrisation = parametrisation;
+	}
+
+	const list<Plan*>& PlanType::getPlans() const
+	{
+		return plans;
+	}
+
+	void PlanType::setPlans(const list<Plan*>& plans)
+	{
+		this->plans = plans;
+	}f
+
 } /* namespace Alica */
+
+
