a4128dd5f358827962812f34f3410aab5b56fc75
Stefan Jakob <stevi872@freenet.de>
2014-06-10 11:46:25 +0200

    bla

---
+++ include/engine/RunningPlan.h
@@ -0,0 +1,23 @@
+/*
+ * RunningPlan.h
+ *
+ *  Created on: Jun 10, 2014
+ *      Author: stefan
+ */
+
+#ifndef RUNNINGPLAN_H_
+#define RUNNINGPLAN_H_
+
+namespace alica
+{
+
+	class RunningPlan
+	{
+	public:
+		RunningPlan();
+		virtual ~RunningPlan();
+	};
+
+} /* namespace alica */
+
+#endif /* RUNNINGPLAN_H_ */
