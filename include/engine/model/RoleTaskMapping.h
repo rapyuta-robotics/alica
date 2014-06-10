9565ff015c38ba437d8c367554fe6fe7b070467c
Stefan Jakob <stevi872@freenet.de>
2014-06-10 11:44:20 +0200

    working on model

--- include/engine/model/RoleTaskMapping.h
+++ include/engine/model/RoleTaskMapping.h
@@ -8,7 +8,14 @@
 #ifndef ROLETASKMAPPING_H_
 #define ROLETASKMAPPING_H_
 
+using namespace std;
+
+#include <map>
+#include <string>
+#include <sstream>
+
 #include "engine/model/AlicaElement.h"
+#include "Role.h"
 
 namespace alica
 {
@@ -16,11 +23,23 @@ namespace alica
 	/*
 	 *
 	 */
+	class Role;
 	class RoleTaskMapping : public AlicaElement
 	{
 	public:
 		RoleTaskMapping();
 		virtual ~RoleTaskMapping();
+
+		string toString();
+
+		const Role* getRole() const;
+		void setRole(const Role* role);
+		const map<long, double>& getTaskPriorities() const;
+		void setTaskPriorities(const map<long, double>& taskPriorities);
+
+	protected:
+		const Role* role;
+		map<long,double> taskPriorities;
 	};
 
 } /* namespace Alica */
