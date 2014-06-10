5a1c31de4c97667d805ae2205952fb478665522a
Stefan Jakob <stevi872@freenet.de>
2014-06-10 11:44:20 +0200

    working on model

--- src/engine/model/Role.cpp
+++ src/engine/model/Role.cpp
@@ -21,4 +21,69 @@ namespace alica
 		// TODO Auto-generated destructor stub
 	}
 
+	double Role::getPriority(long taskId)
+	{
+		if(this->roleTaskMapping->getTaskPriorities().find(taskId) !=  this->roleTaskMapping->getTaskPriorities().end())
+		{
+			return this->roleTaskMapping->getTaskPriorities().find(taskId)->second;
+		}
+		else
+		{
+			cerr << "ROLE DOES NOT HAVE A PRIORITY FOR TASK: " << taskId << endl;
+			throw new exception;
+		}
+	}
+
+	string Role::toString()
+	{
+		stringstream ss;
+		ss << "#Role: " << this->name << " " << this->id << endl;
+		ss << "\t Characteristics: " << this->characteristics.size() << endl;
+		for (map<string, Characteristic*>::const_iterator iter = this->characteristics.begin();
+				iter != this->characteristics.end(); iter++)
+		{
+			ss << "t" << iter->second->getName() << " : " << iter->second->getCapValue().getName() << endl;
+		}
+		ss << endl;
+		ss << "\tRTM TaskPriorities (" << this->roleTaskMapping->getId() << "): "
+				<< this->roleTaskMapping->getTaskPriorities().size() << endl;
+		for (map<long, double>::const_iterator iterator = this->roleTaskMapping->getTaskPriorities().begin();
+				iterator != this->roleTaskMapping->getTaskPriorities().end(); iterator++)
+		{
+			const long l = iterator->first;
+			const double val = iterator->second;
+			ss << "\t" << l << " : " << val << endl;
+		}
+		ss << endl;
+		ss << "#EndRole" << endl;
+		return ss.str();
+	}
+
+//====================== Getter and Setter ==================
+
+	const map<string, Characteristic*>& Role::getCharacteristics() const
+	{
+		return characteristics;
+	}
+
+	const RoleDefinitionSet* Role::getRoleDefinitionSet() const
+	{
+		return roleDefinitionSet;
+	}
+
+	void Role::setRoleDefinitionSet(const RoleDefinitionSet* roleDefinitionSet)
+	{
+		this->roleDefinitionSet = roleDefinitionSet;
+	}
+
+	const RoleTaskMapping* Role::getRoleTaskMapping() const
+	{
+		return roleTaskMapping;
+	}
+
+	void Role::setRoleTaskMapping(const RoleTaskMapping* roleTaskMapping)
+	{
+		this->roleTaskMapping = roleTaskMapping;
+	}
 } /* namespace Alica */
+
--- src/engine/model/RoleTaskMapping.cpp
+++ src/engine/model/RoleTaskMapping.cpp
@@ -12,13 +12,52 @@ namespace alica
 
 	RoleTaskMapping::RoleTaskMapping()
 	{
-		// TODO Auto-generated constructor stub
 
 	}
 
 	RoleTaskMapping::~RoleTaskMapping()
 	{
-		// TODO Auto-generated destructor stub
+	}
+
+	string RoleTaskMapping::toString()
+	{
+		stringstream ss;
+		ss << "#RoleTaskMapping: " << this->name << " " << this->id << endl;
+		ss << "\t Role-Name: " << this->role->getName() << endl;
+		ss << "\t TaskPriorities: " << this->taskPriorities.size() << endl;
+		for(map<long, double>::const_iterator iterator = this->taskPriorities.begin(); iterator != this->taskPriorities.end(); iterator++)
+		{
+			const long l = iterator->first;
+			const double val = iterator->second;
+			ss << "\t" << l << " : " << val << endl;
+		}
+		ss << endl;
+		ss << "#EndRoleTaskMapping" << endl;
+		return ss.str();
+	}
+
+//========================= Getter and Setter ======================
+
+	const Role* RoleTaskMapping::getRole() const
+	{
+		return role;
+	}
+
+	void RoleTaskMapping::setRole(const Role* role)
+	{
+		this->role = role;
+	}
+
+	const map<long, double>& RoleTaskMapping::getTaskPriorities() const
+	{
+		return taskPriorities;
+	}
+
+	void RoleTaskMapping::setTaskPriorities(const map<long, double>& taskPriorities)
+	{
+		this->taskPriorities = taskPriorities;
 	}
 
 } /* namespace Alica */
+
+
