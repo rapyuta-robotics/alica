25a4043975227c3cffcc5a4939e2c8028cc56dc8
Stefan Jakob <stevi872@freenet.de>
2014-06-13 16:20:17 +0200

    working on TeamObserver

---
+++ include/engine/collections/RobotProperties.h
@@ -0,0 +1,36 @@
+/*
+ * RobotProperties.h
+ *
+ *  Created on: Jun 13, 2014
+ *      Author: stefan
+ */
+
+#ifndef ROBOTPROPERTIES_H_
+#define ROBOTPROPERTIES_H_
+
+using namespace std;
+
+#include <string>
+
+namespace alica
+{
+
+	class RobotProperties
+	{
+	public:
+		RobotProperties();
+		RobotProperties(string name);
+		virtual ~RobotProperties();
+		int getId() const;
+		void setId(int id);
+		const string& getName() const;
+		void setName(const string& name);
+
+	protected:
+		int id = -1;
+		string name;
+	};
+
+} /* namespace alica */
+
+#endif /* ROBOTPROPERTIES_H_ */
---
+++ src/engine/logging/Logger.cpp
@@ -0,0 +1,24 @@
+/*
+ * Logger.cpp
+ *
+ *  Created on: Jun 13, 2014
+ *      Author: stefan
+ */
+
+#include <engine/logging/Logger.h>
+
+namespace alica
+{
+
+	Logger::Logger()
+	{
+		// TODO Auto-generated constructor stub
+
+	}
+
+	Logger::~Logger()
+	{
+		// TODO Auto-generated destructor stub
+	}
+
+} /* namespace alica */
