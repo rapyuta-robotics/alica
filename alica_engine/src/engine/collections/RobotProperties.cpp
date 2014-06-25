3d3024992831495f33034080503195573a232865
Stefan Jakob <stevi872@freenet.de>
2014-06-25 15:53:28 +0200

    working on collections

--- alica_engine/src/engine/collections/RobotProperties.cpp
+++ alica_engine/src/engine/collections/RobotProperties.cpp
@@ -6,29 +6,67 @@
  */
 
 #include <engine/collections/RobotProperties.h>
+#include <engine/model/Characteristic.h>
+#include <engine/AlicaEngine.h>
+#include <engine/PlanRepository.h>
+#include <engine/model/CapValue.h>
+
 
 namespace alica
 {
 
 	RobotProperties::RobotProperties()
 	{
-		 //TODO Auto-generated constructor stub
+	}
 
+	RobotProperties::RobotProperties(string name)
+	{
+		supplementary::SystemConfig* sc = supplementary::SystemConfig::getInstance();
+		this->id = (*sc)["Globals"]->tryGet<int>(-1,"Globals", "Team", name.c_str(), "ID", NULL);
+		this->name = name;
+		this->capabilities = AlicaEngine::getInstance()->getPlanRepository()->getCapabilities();
+		string key = "";
+		string kvalue = "";
+		auto caps = (*sc)["Globals"]->getNames("Globals", "Team", this->name.c_str(), NULL);
+		for(string s : *caps)
+		{
+			if(s.compare("ID") == 0 || s.compare("DefaultRole"))
+			{
+				continue;
+			}
+			key = s;
+			kvalue = (*sc)["Globals"]->get<string>("Globals","Team",this->name.c_str(),s.c_str(), NULL);
+			for(auto p : this->capabilities)
+			{
+				if(p.second->getName().compare(key) == 0)
+				{
+					for(CapValue* val : p.second->getCapValues())
+					{
+
+						string k = "";
+						transform(kvalue.begin(), kvalue.end(), k.begin(), ::tolower);
+						if(val->getName().compare(k) == 0)
+						{
+							Characteristic* cha = new Characteristic();
+							cha->setCapability(p.second);
+							cha->setCapValue(val);
+							this->getCharacteristics().insert(pair<string, Characteristic*>(key, cha));
+						}
+					}
+				}
+
+			}
+		}
+		this->defaultRole = (*sc)["Globals"]->tryGet<string>("Attack", "Globals", "Team", name.c_str(), "DefaultRole");
 	}
 
 	RobotProperties::~RobotProperties()
 	{
-		// TODO Auto-generated destructor stub
 	}
 
 	int RobotProperties::getId() const
 	{
 		return id;
-	}
-
-	RobotProperties::RobotProperties(string name)
-	{
-		this->name = name;
 	}
 
 	void RobotProperties::setId(int id)
@@ -44,6 +82,31 @@ namespace alica
 	void RobotProperties::setName(const string& name)
 	{
 		this->name = name;
+	}
+
+	map<string, Characteristic*> RobotProperties::getCharacteristics()
+	{
+		return characteristics;
+	}
+
+	const string& RobotProperties::getDefaultRole() const
+	{
+		return defaultRole;
+	}
+
+	void RobotProperties::setDefaultRole(const string& defaultRole)
+	{
+		this->defaultRole = defaultRole;
+	}
+
+	string RobotProperties::toString()
+	{
+		stringstream ss;
+		ss << "[RobotProperties: Id=" << this->getId() << " Default Role: " << this->getDefaultRole() << endl;
+		for(pair<string, Characteristic*> p : this->getCharacteristics()) {
+			ss << p.first << " = " << p.second->getCapValue()->getName() << endl;
+		}
+		return ss.str();
 	}
 
 } /* namespace alica */
