71e80431f015740ec578c6d585590dd0c8455792
Stephan Opfer <opfer@vs.uni-kassel.de>
2014-06-26 11:37:18 +0200

    - introduced more features into the test plans for the plan parser test

---
+++ alica_engine/test/etc/Expr/constraints/MidFieldStandard1402488712657BehaviourConstraints.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Alica;
+using AD = AutoDiff;
+
+/*PROTECTED REGION ID(ch1402488712657) ENABLED START*/
+//Add additional using directives here
+/*PROTECTED REGION END*/
+
+namespace Alica.Validators {
+	public partial class ConstraintFunctions { //1402488712657 {
+
+		//BehaviourConfiguration: NewBehaviourDefault 1402488712657
+
+	}
+}
