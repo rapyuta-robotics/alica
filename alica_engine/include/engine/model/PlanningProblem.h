3bf7448785d2d388d0d96ca0a576e60b7d876168
ppa <p.panin@gmx.de>
2014-07-02 17:10:45 +0200

    added PlanningProblem to planParser

---
+++ alica_engine/include/engine/model/PlanningProblem.h
@@ -0,0 +1,74 @@
+/*
+ * PlanningProblem.h
+ *
+ *  Created on: Jul 1, 2014
+ *      Author: Paul Panin
+ */
+
+#ifndef PLANNINGPROBLEM_H_
+#define PLANNINGPROBLEM_H_
+
+
+#include <list>
+#include "Plan.h"
+#include "PreCondition.h"
+#include "PostCondition.h"
+#include "RuntimeCondition.h"
+#include "PlanningType.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+namespace alica
+{
+
+	class PlanningProblem : public AlicaElement
+	{
+	public:
+		PlanningProblem();
+		virtual ~PlanningProblem();
+		const Plan* getAlternativePlan() const;
+		void setAlternativePlan(Plan* alternativePlan);
+		bool isDistributeProblem() const;
+		void setDistributeProblem(bool distributeProblem);
+		const string& getFileName() const;
+		void setFileName(const string& fileName);
+		PlanningType getPlanningType() const;
+		void setPlanningType(PlanningType planningType);
+		list<AbstractPlan*>& getPlans();
+		void setPlans(list<AbstractPlan*>& plans);
+		const PostCondition* getPostCondition() const;
+		void setPostCondition(PostCondition* postCondition);
+		const PreCondition* getPreCondition() const;
+		void setPreCondition( PreCondition* preCondition);
+		const string& getRequirements() const;
+		void setRequirements(const string& requirements);
+		const RuntimeCondition* getRuntimeCondition() const;
+		void setRuntimeCondition(RuntimeCondition* runtimeCondition);
+		int getUpdateRate() const;
+		void setUpdateRate(int updateRate);
+		const Plan* getWaitPlan() const;
+		void setWaitPlan(Plan* waitPlan);
+
+		list<AbstractPlan*> plans;
+		Plan* alternativePlan;
+		Plan* waitPlan;
+		int updateRate;
+		bool distributeProblem;
+		PlanningType planningType;
+		string requirements;
+		string fileName;
+		PostCondition* postCondition;
+		PreCondition* preCondition;
+		RuntimeCondition* runtimeCondition;
+
+
+
+
+
+	};
+
+} /* namespace supplementary */
+
+#endif /* PLANNINGPROBLEM_H_ */
