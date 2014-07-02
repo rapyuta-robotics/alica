f5701741cfc43876a73b399328e695dae0ab3e5e
ppa <p.panin@gmx.de>
2014-07-02 17:10:45 +0200

    added PlanningProblem to planParser

---
+++ alica_engine/src/engine/model/PlanningProblem.cpp
@@ -0,0 +1,134 @@
+/*
+ * PlanningProblem.cpp
+ *
+ *  Created on: Jul 1, 2014
+ *      Author: Paul Panin
+ */
+
+#include "engine/model/PlanningProblem.h"
+
+namespace alica
+{
+
+	PlanningProblem::PlanningProblem()
+	{
+		// TODO Auto-generated constructor stub
+
+	}
+
+	PlanningProblem::~PlanningProblem()
+	{
+		// TODO Auto-generated destructor stub
+	}
+
+	const Plan* PlanningProblem::getAlternativePlan() const
+	{
+		return alternativePlan;
+	}
+
+	void PlanningProblem::setAlternativePlan(Plan* alternativePlan)
+	{
+		this->alternativePlan = alternativePlan;
+	}
+
+	bool PlanningProblem::isDistributeProblem() const
+	{
+		return distributeProblem;
+	}
+
+	void PlanningProblem::setDistributeProblem(bool distributeProblem)
+	{
+		this->distributeProblem = distributeProblem;
+	}
+
+	const string& PlanningProblem::getFileName() const
+	{
+		return fileName;
+	}
+
+	void PlanningProblem::setFileName(const string& fileName)
+	{
+		this->fileName = fileName;
+	}
+
+	PlanningType PlanningProblem::getPlanningType() const
+	{
+		return planningType;
+	}
+
+	void PlanningProblem::setPlanningType(PlanningType planningType)
+	{
+		this->planningType = planningType;
+	}
+
+	list<AbstractPlan*>& PlanningProblem::getPlans()
+	{
+		return plans;
+	}
+
+	void PlanningProblem::setPlans(list<AbstractPlan*>& plans)
+	{
+		this->plans = plans;
+	}
+
+	const PostCondition* PlanningProblem::getPostCondition() const
+	{
+		return postCondition;
+	}
+
+	void PlanningProblem::setPostCondition(PostCondition* postCondition)
+	{
+		this->postCondition = postCondition;
+	}
+
+	const PreCondition* PlanningProblem::getPreCondition() const
+	{
+		return preCondition;
+	}
+
+	void PlanningProblem::setPreCondition(PreCondition* preCondition)
+	{
+		this->preCondition = preCondition;
+	}
+
+	const string& PlanningProblem::getRequirements() const
+	{
+		return requirements;
+	}
+
+	void PlanningProblem::setRequirements(const string& requirements)
+	{
+		this->requirements = requirements;
+	}
+
+	const RuntimeCondition* PlanningProblem::getRuntimeCondition() const
+	{
+		return runtimeCondition;
+	}
+
+	void PlanningProblem::setRuntimeCondition(RuntimeCondition* runtimeCondition)
+	{
+		this->runtimeCondition = runtimeCondition;
+	}
+
+	int PlanningProblem::getUpdateRate() const
+	{
+		return updateRate;
+	}
+
+	void PlanningProblem::setUpdateRate(int updateRate)
+	{
+		this->updateRate = updateRate;
+	}
+
+	const Plan* PlanningProblem::getWaitPlan() const
+	{
+		return waitPlan;
+	}
+
+	void PlanningProblem::setWaitPlan(Plan* waitPlan)
+	{
+		this->waitPlan = waitPlan;
+	}
+
+} /* namespace supplementary */
