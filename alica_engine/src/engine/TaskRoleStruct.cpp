6863dea88cd8eaff71b391ae5032736777bc1209
Stefan Jakob <stevi872@freenet.de>
2014-07-09 14:02:09 +0200

    added taskRoleStruct

---
+++ alica_engine/src/engine/TaskRoleStruct.cpp
@@ -0,0 +1,34 @@
+/*
+ * TaskRoleStruct.cpp
+ *
+ *  Created on: Jul 9, 2014
+ *      Author: Stefan Jakob
+ */
+
+#include <engine/TaskRoleStruct.h>
+
+namespace alica
+{
+
+
+	TaskRoleStruct::TaskRoleStruct(long tid, long rid)
+	{
+		taskId = tid;
+		roleId = rid;
+	}
+
+	TaskRoleStruct::~TaskRoleStruct()
+	{
+	}
+
+	bool TaskRoleStruct::equals(TaskRoleStruct* trs)
+	{
+		return taskId==trs->taskId && roleId==trs->roleId;
+	}
+
+	int TaskRoleStruct::getHashCode()
+	{
+		return hash<long>(taskId) ^ hash<long>(roleId);
+	}
+
+} /* namespace alica */
