204f67de56244adfc6afa18066cac39210605f6c
Stefan Jakob <stevi872@freenet.de>
2014-07-21 13:07:40 +0200

    finished allocationDifference

--- alica_engine/include/engine/allocationauthority/AllocationDifference.h
+++ alica_engine/include/engine/allocationauthority/AllocationDifference.h
@@ -8,16 +8,59 @@
 #ifndef ALLOCATIONDIFFERENCE_H_
 #define ALLOCATIONDIFFERENCE_H_
 
+using namespace std;
+
+#include <vector>
+#include <string>
+#include <sstream>
+#include <algorithm>
+
 namespace alica
 {
+
+	class EntryPointRobotPair;
 
 	class AllocationDifference
 	{
 	public:
 		AllocationDifference();
 		virtual ~AllocationDifference();
+		enum Reason {message, utility, empty};
+		AllocationDifference::Reason getReason();
+		void setReason(AllocationDifference::Reason reason);
+		bool isEmpty();
+		void reset();
+		bool equals(AllocationDifference* other);
+		void applyDifference(AllocationDifference* other);
+		string toString();
+
+	private:
+		vector<EntryPointRobotPair*> additions;
+		vector<EntryPointRobotPair*> subtractions;
+
+	protected:
+		AllocationDifference::Reason reason;
+
 	};
 
 } /* namespace alica */
+
+
+//TODO this about other hash calculation
+namespace std
+{
+    template<>
+    struct hash<alica::AllocationDifference>
+    {
+        typedef alica::AllocationDifference argument_type;
+        typedef std::size_t value_type;
+
+        value_type operator()(argument_type & ad) const
+        {
+            value_type const h1 ( std::hash<std::string>()(ad.toString()) );
+            return h1;
+        }
+    };
+}
 
 #endif /* ALLOCATIONDIFFERENCE_H_ */
