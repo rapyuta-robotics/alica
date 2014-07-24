4ac260ac55961c328ead6f7d8ddf335828d06f9d
Stefan Jakob <stevi872@freenet.de>
2014-07-24 11:40:05 +0200

    working on cyclemanager

--- alica_engine/include/engine/allocationauthority/CycleManager.h
+++ alica_engine/include/engine/allocationauthority/CycleManager.h
@@ -7,21 +7,66 @@
 
 #ifndef CYCLEMANAGER_H_
 #define CYCLEMANAGER_H_
+#define CM_DEBUG
 
+using namespace std;
+
+#include <vector>
+
+#include "engine/allocationauthority/AllocationDifference.h"
+namespace supplementary
+{
+	class SystemConfig;
+}
 namespace alica
 {
 	class RunningPlan;
+	class PlanRepository;
+	struct AllocationAuthorityInfo;
+	class Assignment;
 
 	class CycleManager
 	{
 	public:
 		CycleManager(RunningPlan* p);
 		virtual ~CycleManager();
-		//TODO nicht implementiert
 		void update();
 		bool isOverridden();
 		bool setAssignment(RunningPlan* r);
 		bool mayDoUtilityCheck();
+		void setNewAllocDiff(RunningPlan* curP, AllocationDifference* aldif);
+		void setNewAllocDiff(RunningPlan* curP, Assignment* oldAss, Assignment* newAss, AllocationDifference::Reason reas);
+		void handleAuthorityInfo(AllocationAuthorityInfo* aai);
+		bool needsSending();
+		void sent();
+		bool haveAuthority();
+
+
+	protected:
+		static supplementary::SystemConfig* sc;
+		static int maxAllocationCycles;
+		static bool enabled;
+		vector<AllocationDifference*> allocationHistory;
+		PlanRepository* pr;
+		int newestAllocationDifference;
+		int myID;
+		enum CycleState
+		{
+			observing, overridden, overriding
+		};
+		unsigned long ovverideTimestamp;
+		double intervalIncFactor;
+		double intervalDecFactor;
+		static unsigned long minimalOverrideTImeInterval;
+		static unsigned long maximalOverrideTimeInterval;
+		static unsigned long overrideShoutInterval;
+		static unsigned long overrideWaitInterval;
+		unsigned long overrideShoutTime;
+		static int historySize;
+		CycleState state;
+		RunningPlan* rp;
+		AllocationAuthorityInfo* fixedAllocation;
+		bool detectAllocationCycle();
 	};
 
 } /* namespace supplementary */
--- alica_engine/src/engine/AllocationAuthority/CycleManager.cpp
+++ alica_engine/src/engine/AllocationAuthority/CycleManager.cpp
@@ -7,18 +7,47 @@
 
 #include "engine/allocationauthority/CycleManager.h"
 
+#include <SystemConfig.h>
+#include "engine/AlicaEngine.h"
+#include "engine/allocationauthority/AllocationDifference.h"
+#include "engine/ITeamObserver.h"
+#include "engine/IAlicaClock.h"
+
 namespace alica
 {
+	supplementary::SystemConfig* CycleManager::sc = supplementary::SystemConfig::getInstance();
+	int CycleManager::maxAllocationCycles = (*sc)["alica"]->get<int>("Alica", "CycleDetection", "CycleCount");
+	bool CycleManager::enabled = (*sc)["alica"]->get<bool>("Alica", "CycleDetection", "Enabled");
+	unsigned long CycleManager::minimalOverrideTImeInterval = (*sc)["alica"]->get<unsigned long>(
+			"Alica", "CycleDetection", "MinimalAuthorityTimeInterval") * 1000000;
+	unsigned long CycleManager::maximalOverrideTimeInterval = (*sc)["alica"]->get<unsigned long>(
+			"Alica", "CycleDetection", "MaximalAuthorityTimeInterval") * 1000000;
+	unsigned long CycleManager::overrideShoutInterval = (*sc)["alica"]->get<unsigned long>("Alica", "CycleDetection",
+																							"MessageTimeInterval")
+			* 1000000;
+	unsigned long CycleManager::overrideWaitInterval = (*sc)["alica"]->get<unsigned long>("Alica", "CycleDetection",
+																							"MessageWaitTimeInterval")
+			* 1000000;
+	int CycleManager::historySize = (*sc)["alica"]->get<int>("Alica", "CycleDetection", "HistorySize");
 
 	CycleManager::CycleManager(RunningPlan* p)
 	{
-		// TODO Auto-generated constructor stub
-
+		this->intervalIncFactor = (*sc)["alica"]->get<double>("Alica", "CycleDetection", "IntervalIncreaseFactor");
+		this->intervalDecFactor = (*sc)["alica"]->get<double>("Alica", "CycleDetection", "IntervalDecreaseFactor");
+		this->allocationHistory = vector<AllocationDifference*>(this->historySize);
+		for (int i = 0; i < this->allocationHistory.size(); i++)
+		{
+			this->allocationHistory[i] = new AllocationDifference();
+		}
+		this->newestAllocationDifference = 0;
+		this->state = CycleState::observing;
+		this->rp = p;
+		this->myID = AlicaEngine::getInstance()->getTeamObserver()->getOwnId();
+		this->pr = AlicaEngine::getInstance()->getPlanRepository();
 	}
 
 	CycleManager::~CycleManager()
 	{
-		// TODO Auto-generated destructor stub
 	}
 
 	void CycleManager::update()
@@ -27,15 +56,48 @@ namespace alica
 	}
 	bool CycleManager::isOverridden()
 	{
-		//TODO:
+		return this->state==CycleState::overridden && this->fixedAllocation != nullptr;
 	}
+
+	void alica::CycleManager::setNewAllocDiff(RunningPlan* curP, AllocationDifference* aldif)
+	{
+	}
+
+	void alica::CycleManager::setNewAllocDiff(RunningPlan* curP, Assignment* oldAss, Assignment* newAss,
+												AllocationDifference::Reason reas)
+	{
+	}
+
+	void alica::CycleManager::handleAuthorityInfo(AllocationAuthorityInfo* aai)
+	{
+	}
+
+	bool alica::CycleManager::needsSending()
+	{
+		return this->state==CycleState::overriding && (this->overrideShoutTime + overrideShoutInterval < AlicaEngine::getInstance()->getIAlicaClock()->now());
+	}
+
+	void alica::CycleManager::sent()
+	{
+		this->overrideShoutTime = AlicaEngine::getInstance()->getIAlicaClock()->now();
+	}
+
+	bool alica::CycleManager::haveAuthority()
+	{
+		return this->state == CycleState::overriding;
+	}
+
 	bool CycleManager::setAssignment(RunningPlan* r)
 	{
 		//TODO:
 	}
 	bool CycleManager::mayDoUtilityCheck()
 	{
-		//TODO:
+		return this->state != CycleState::overridden;
+	}
+
+	bool CycleManager::detectAllocationCycle()
+	{
 	}
 
 } /* namespace supplementary */
