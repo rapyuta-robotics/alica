f1ca9490eb1887c34c2b9ffd421641a47f938dc4
ppa <p.panin@gmx.de>
2014-08-27 11:16:40 +0200

    rename folders and create syncmodul package

---
+++ alica_engine/src/engine/syncmodul/SyncModul.cpp
@@ -0,0 +1,24 @@
+/*
+ * SyncModul.cpp
+ *
+ *  Created on: Aug 27, 2014
+ *      Author: Paul Panin
+ */
+
+#include "engine/syncmodul/SyncModul.h"
+
+namespace supplementary
+{
+
+	SyncModul::SyncModul()
+	{
+		// TODO Auto-generated constructor stub
+
+	}
+
+	SyncModul::~SyncModul()
+	{
+		// TODO Auto-generated destructor stub
+	}
+
+} /* namespace supplementary */
