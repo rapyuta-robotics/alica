7efae3b427ba8ae88a3555354dbeb2af8bc7bb5f
Endy <awi@vs.uni-kassel.de>
2014-10-01 15:51:03 +0200

    Ros communication proxy is done

--- alica_ros_proxy/include/communication/AlicaRosCommunication.h
+++ alica_ros_proxy/include/communication/AlicaRosCommunication.h
@@ -8,11 +8,17 @@
 #ifndef ALICAROSCOMMUNICATION_H_
 #define ALICAROSCOMMUNICATION_H_
 
-
 #include "engine/IAlicaCommunication.h"
+#include "ros/ros.h"
+
+#include "alica_ros_proxy/AllocationAuthorityInfo.h"
+#include "alica_ros_proxy/BehaviourEngineInfo.h"
+#include "alica_ros_proxy/PlanTreeInfo.h"
+#include "alica_ros_proxy/RoleSwitch.h"
+#include "alica_ros_proxy/SyncReady.h"
+#include "alica_ros_proxy/SyncTalk.h"
 
 using namespace alica;
-
 
 namespace alicaRosProxy
 {
@@ -25,12 +31,36 @@ namespace alicaRosProxy
 
 		virtual void tick();
 
-		virtual void sendAllocationAuthority(AllocationAuthorityInfo aai);
-		virtual void sendBehaviourEngineInfo(BehaviourEngineInfo bi);
-		virtual void sendPlanTreeInfo(PlanTreeInfo pti);
-		virtual void sendRoleSwitch(RoleSwitch rs);
-		virtual void sendSyncReady(SyncReady sr);
-		virtual void sendSyncTalk(SyncTalk st);
+		virtual void sendAllocationAuthority(AllocationAuthorityInfo& aai);
+		virtual void sendBehaviourEngineInfo(BehaviourEngineInfo& bi);
+		virtual void sendPlanTreeInfo(PlanTreeInfo& pti);
+		virtual void sendRoleSwitch(RoleSwitch& rs);
+		virtual void sendSyncReady(SyncReady& sr);
+		virtual void sendSyncTalk(SyncTalk& st);
+
+		virtual void handleAllocationAuthorityRos(alica_ros_proxy::AllocationAuthorityInfoPtr aai);
+		virtual void handlePlanTreeInfoRos(alica_ros_proxy::PlanTreeInfoPtr pti);
+		virtual void handleSyncReadyRos(alica_ros_proxy::SyncReadyPtr sr);
+		virtual void handleSyncTalkRos(alica_ros_proxy::SyncTalkPtr st);
+
+	protected:
+		int ownID;
+		ros::NodeHandle rosNode;
+		ros::AsyncSpinner* spinner;
+
+		ros::Publisher BehaviourEngineInfoPublisher;
+		ros::Publisher RoleSwitchPublisher;
+
+		ros::Publisher AllocationAuthorityInfoPublisher;
+		ros::Subscriber AllocationAuthorityInfoSubscriber;
+
+		ros::Publisher PlanTreeInfoPublisher;
+		ros::Subscriber PlanTreeInfoSubscriber;
+
+		ros::Publisher SyncReadyPublisher;
+		ros::Subscriber SyncReadySubscriber;
+		ros::Publisher SyncTalkPublisher;
+		ros::Subscriber SyncTalkSubscriber;
 	};
 
 } /* namespace alicaRosProxy */
