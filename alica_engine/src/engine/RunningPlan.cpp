f63f2e36a881a8eafca70040d3c07383f43b9d03
Stefan Jakob <stevi872@freenet.de>
2014-10-09 13:07:21 +0200

    replaced unsigned long with alicaTime

--- alica_engine/src/engine/RunningPlan.cpp
+++ alica_engine/src/engine/RunningPlan.cpp
@@ -387,12 +387,12 @@ namespace alica
 		this->assignment = assignment;
 	}
 
-	unsigned long RunningPlan::getPlanStartTime()
+	alicaTime RunningPlan::getPlanStartTime()
 	{
 		return planStartTime;
 	}
 
-	unsigned long RunningPlan::getStateStartTime()
+	alicaTime RunningPlan::getStateStartTime()
 	{
 		return stateStartTime;
 	}
@@ -846,7 +846,7 @@ namespace alica
 	}
 
 	bool RunningPlan::recursiveUpdateAssignment(list<shared_ptr<SimplePlanTree> > spts, vector<int> availableAgents,
-												list<int> noUpdates, unsigned long now)
+												list<int> noUpdates, alicaTime now)
 	{
 		if (this->isBehaviour())
 		{
