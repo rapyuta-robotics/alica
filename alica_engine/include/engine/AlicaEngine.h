20a683a6d3b7ee164ed7cef6940344284616bf51
Endy <awi@vs.uni-kassel.de>
2014-10-27 18:46:17 +0100

    private desctructor?

--- alica_engine/include/engine/AlicaEngine.h
+++ alica_engine/include/engine/AlicaEngine.h
@@ -83,6 +83,8 @@ namespace alica
 		void stepNotify();
 		PlanBase* getPlanBase();
 
+		~AlicaEngine();
+
 		/**
 		 * Switch the engine between normal operation and silent mode, in which no messages other than debugging information are sent out.
 		 * This is useful for a robot on hot standby.
@@ -108,7 +110,6 @@ namespace alica
 
 	private:
 
-		~AlicaEngine();
 		/**
 		 * Set to have the engine's main loop wait on a signal via MayStep
 		 */
