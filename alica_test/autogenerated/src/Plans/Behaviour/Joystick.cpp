6247e2dee457d79593305afe16b4ece0527c7d09
ppa <p.panin@gmx.de>
2015-02-03 14:55:01 +0100

    Beh for test

---
+++ alica_test/autogenerated/src/Plans/Behaviour/Joystick.cpp
@@ -0,0 +1,38 @@
+using namespace std;
+#include "Plans/Behaviour/Joystick.h"
+
+/*PROTECTED REGION ID(inccpp1421854685673) ENABLED START*/ //Add additional includes here
+#include <msl_msgs/JoystickCommand.h>
+#include <msl_actuator_msgs/KickControl.h>
+#include <msl_actuator_msgs/BallHandleCmd.h>
+#include <msl_actuator_msgs/MotionControl.h>
+#include <msl_actuator_msgs/KickControl.h>
+/*PROTECTED REGION END*/
+namespace alica
+{
+    /*PROTECTED REGION ID(staticVars1421854685673) ENABLED START*/ //initialise static variables here
+    /*PROTECTED REGION END*/
+    Joystick::Joystick() :
+            DomainBehaviour("Joystick")
+    {
+        /*PROTECTED REGION ID(con1421854685673) ENABLED START*/ //Add additional options here
+        /*PROTECTED REGION END*/
+    }
+    Joystick::~Joystick()
+    {
+        /*PROTECTED REGION ID(dcon1421854685673) ENABLED START*/ //Add additional options here
+        /*PROTECTED REGION END*/
+    }
+    void Joystick::run(void* msg)
+    {
+        /*PROTECTED REGION ID(run1421854685673) ENABLED START*/ //Add additional options here
+        /*PROTECTED REGION END*/
+    }
+    void Joystick::initialiseParameters()
+    {
+        /*PROTECTED REGION ID(initialiseParameters1421854685673) ENABLED START*/ //Add additional options here
+        /*PROTECTED REGION END*/
+    }
+/*PROTECTED REGION ID(methods1421854685673) ENABLED START*/ //Add additional methods here
+/*PROTECTED REGION END*/
+} /* namespace alica */
