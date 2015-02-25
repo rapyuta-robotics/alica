a54a467205c0e98918f46305ef0ef43317ad3de1
ppa <p.panin@gmx.de>
2015-02-25 10:57:49 +0100

    Change of method name

--- alica-essentials/system_config/src/Configuration.cpp
+++ alica-essentials/system_config/src/Configuration.cpp
@@ -513,7 +513,7 @@ namespace supplementary
 	 */
 	shared_ptr<vector<string> > Configuration::getParams(char seperator, const char *path, va_list ap)
 	{
-		shared_ptr<vector<string> > params(new vector<string>());
+		shared_ptr<vector<string> > params = make_shared <vector<string> >();
 		if (path != NULL)
 		{
 			const char *temp = path;
