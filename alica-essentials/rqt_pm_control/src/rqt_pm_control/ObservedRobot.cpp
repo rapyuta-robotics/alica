8d1c71250d4ee1b3507073a471bddd243d7fba79
Stephan Opfer <opfer@vs.uni-kassel.de>
2015-02-27 10:48:05 +0100

    added new files

---
+++ alica-essentials/rqt_pm_control/src/rqt_pm_control/ObservedRobot.cpp
@@ -0,0 +1,24 @@
+/*
+ * ObservedRobot.cpp
+ *
+ *  Created on: Feb 27, 2015
+ *      Author: Stephan Opfer
+ */
+
+#include "ObservedRobot.h"
+
+namespace rqt_pm_control
+{
+
+	ObservedRobot::ObservedRobot(string robotName, int robotId) : RobotMetaData(robotName, robotId)
+	{
+		// TODO Auto-generated constructor stub
+
+	}
+
+	ObservedRobot::~ObservedRobot()
+	{
+		// TODO Auto-generated destructor stub
+	}
+
+} /* namespace rqt_pm_control */
