5170d7ff2965a5db4ad789a251d44969b8558fc7
Tobias <Tobi_80@gmx.net>
2015-04-08 18:16:48 +0200

    added condition timeout

--- alica_engine/include/engine/BasicCondition.h
+++ alica_engine/include/engine/BasicCondition.h
@@ -23,6 +23,10 @@ namespace alica
 		BasicCondition();
 		virtual ~BasicCondition();
 		virtual bool evaluate(shared_ptr<RunningPlan> rp) = 0;
+
+		bool isStateTimedOut(unsigned long timeOut, shared_ptr<RunningPlan> rp);
+		bool isTimeOut(unsigned long timeOut, unsigned long startTime, shared_ptr<RunningPlan> rp);
+
 	};
 
 } /* namespace alica */
--- alica_engine/src/engine/BasicCondition.cpp
+++ alica_engine/src/engine/BasicCondition.cpp
@@ -6,16 +6,37 @@
  */
 
 #include <engine/BasicCondition.h>
+#include <engine/AlicaEngine.h>
+#include <engine/RunningPlan.h>
 
-namespace alica
-{
+namespace alica {
 
-	BasicCondition::BasicCondition()
-	{
+BasicCondition::BasicCondition() {
+}
+
+BasicCondition::~BasicCondition() {
+}
+
+bool BasicCondition::isStateTimedOut(unsigned long timeOut, shared_ptr<RunningPlan> rp) {
+	if (rp->getStateStartTime() == 0)
+		return false;
+	long time = (long) (rp->getAlicaEngine()->getIAlicaClock()->now());
+	long timeDiff = time - (long) (rp->getStateStartTime());
+	if (timeDiff > timeOut) {
+		return true;
 	}
+	return false;
+}
 
-	BasicCondition::~BasicCondition()
-	{
+bool BasicCondition::isTimeOut(unsigned long timeOut, unsigned long startTime, shared_ptr<RunningPlan> rp) {
+
+	if (startTime == 0)
+		return false;
+	long time = (long) (rp->getAlicaEngine()->getIAlicaClock()->now());
+	long timeDiff = time - (long) (startTime);
+	if (timeDiff > timeOut) {
+		return true;
 	}
-
+	return false;
+}
 } /* namespace alica */
