cb342f5f6913375b314adf78ea56914b963eebac
Stephan Opfer <opfer@vs.uni-kassel.de>
2015-09-08 15:47:42 +0200

    fix infinite recursion

--- alica_dummy_proxy/src/clock/AlicaSystemClock.cpp
+++ alica_dummy_proxy/src/clock/AlicaSystemClock.cpp
@@ -7,6 +7,7 @@
 
 #include "clock/AlicaSystemClock.h"
 #include <time.h>
+#include <unistd.h>
 
 namespace alica_dummy_proxy {
 
@@ -20,22 +21,20 @@ namespace alica_dummy_proxy {
 	{
 		// TODO Auto-generated destructor stub
 	}
+
 	alica::AlicaTime AlicaSystemClock::now()
 	{
 		time_t t;
 		t = time(NULL);
 		alica::AlicaTime ret = (alica::AlicaTime)(t * 1000000000UL + t*1000*1000);
 		return ret;
-
-
-
 	}
+
 	void AlicaSystemClock::sleep(long us)
 	{
 		int sec = us/1000000;
 		int nsec = (us%1000000)*1000;
-		sleep(sec*1000);
-
+		::sleep(sec*1000);
 	}
 
 } /* namespace alica */
