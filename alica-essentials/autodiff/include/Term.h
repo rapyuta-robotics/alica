31c24e1e957804e2e2a08c77fab9c9d781d7a813
Tobias <Tobi_80@gmx.net>
2015-12-04 14:25:46 +0100

    added &= to Term but not tested

--- alica-essentials/autodiff/include/Term.h
+++ alica-essentials/autodiff/include/Term.h
@@ -106,6 +106,9 @@ namespace autodiff
 	shared_ptr<Term> operator<(const shared_ptr<Term>& left, const shared_ptr<Term>& right);
 	shared_ptr<Term> operator<=(const shared_ptr<Term>& left, const shared_ptr<Term>& right);
 	shared_ptr<Term> operator>=(const shared_ptr<Term>& left, const shared_ptr<Term>& right);
+
+	shared_ptr<Term> operator&=(const shared_ptr<Term>& left, const shared_ptr<Term>& right);
+
 } /* namespace autodiff */
 
 #endif /* TERM_H_ */
