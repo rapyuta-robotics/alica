a775d9d0bbd47d3aa20348f7372d3d6d4aba333f
Stephan Opfer <opfer@vs.uni-kassel.de>
2016-05-09 19:56:58 +0200

    added header for custom hash functions

---
+++ alica-essentials/system_util/include/CustomHashes.h
@@ -0,0 +1,27 @@
+/*
+ * CustomHashes.h
+ *
+ *  Created on: 9 May 2016
+ *      Author: Stephan Opfer
+ */
+
+#ifndef SRC_CUSTOMHASHES_H_
+#define SRC_CUSTOMHASHES_H_
+
+#include <stdint.h>
+
+namespace supplementary
+{
+
+	class CustomHashes
+	{
+	public:
+
+		static const uint64_t  FNV_MAGIC_PRIME = 0x00000100000001b3; /**< Constant prime for hashing 64bit wise according to FNV Hash Algorithm */
+		static const uint64_t  FNV_OFFSET       = 0xcbf29ce484222325; /**< Constant offset for hashing 64bit wise according to FNV Hash Algorithm */
+
+	};
+
+} /* namespace supplementary */
+
+#endif /* SRC_CUSTOMHASHES_H_ */
