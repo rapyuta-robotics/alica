8217c1eb824280ef0393775884eca06fb069ecb6
Stephan Opfer <opfer@vs.uni-kassel.de>
2017-09-19 14:28:34 +0200

    - initila commit for robotid

---
+++ alica_engine/include/engine/IRobotID.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace alica
+{
+
+class IRobotID
+{
+  public:
+    IRobotID();
+    virtual ~IRobotID();
+
+    // todo: compare, to string, create (char*, size)
+};
+
+} /* namespace alica */
---
+++ alica_engine/src/engine/IRobotID.cpp
@@ -0,0 +1,16 @@
+#include "IRobotID.h"
+
+namespace alica
+{
+
+IRobotID::IRobotID()
+{
+    // TODO Auto-generated constructor stub
+}
+
+IRobotID::~IRobotID()
+{
+    // TODO Auto-generated destructor stub
+}
+
+} /* namespace alica */
