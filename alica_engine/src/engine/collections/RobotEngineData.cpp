c45254228b652d6432479546245e22f4b1cf6889
Hendrik <hendrik.skubch@rapyuta-robotics.com>
2018-04-23 17:58:54 +0900

    bug fixes

--- alica_engine/src/engine/collections/RobotEngineData.cpp
+++ alica_engine/src/engine/collections/RobotEngineData.cpp
@@ -1,56 +1,61 @@
 #include "engine/collections/RobotEngineData.h"
 
 #include "engine/AlicaEngine.h"
-#include "engine/parser/PlanParser.h"
 #include "engine/PlanRepository.h"
 #include "engine/collections/RobotProperties.h"
 #include "engine/collections/SuccessMarks.h"
 #include "engine/model/AlicaElement.h"
+#include "engine/model/DomainVariable.h"
 #include "engine/model/ForallAgents.h"
 #include "engine/model/Quantifier.h"
-#include "engine/model/DomainVariable.h"
+#include "engine/parser/PlanParser.h"
 
 #include <supplementary/AgentID.h>
 
-#include <typeinfo>
 #include <assert.h>
+#include <typeinfo>
 
-namespace alica {
+namespace alica
+{
 
 /**
  * Basic constructor
  */
 RobotEngineData::RobotEngineData(const AlicaEngine* engine, const supplementary::AgentID* agentId)
-        : _engine(engine)
-        , _agentId(agentId)
-        , _successMarks(std::make_shared<SuccessMarks>(engine)) {
+    : _engine(engine)
+    , _agentId(agentId)
+    , _successMarks(std::make_shared<SuccessMarks>(engine))
+{
     initDomainVariables();
 }
 
-RobotEngineData::~RobotEngineData() {
+RobotEngineData::~RobotEngineData()
+{
     for (auto x : _domainVariables) {
         delete x.second;
     }
 }
 
-void RobotEngineData::setSuccessMarks(shared_ptr<SuccessMarks> successMarks) {
+void RobotEngineData::setSuccessMarks(shared_ptr<SuccessMarks> successMarks)
+{
     _successMarks = successMarks;
 }
 
-void RobotEngineData::initDomainVariables() {
+void RobotEngineData::initDomainVariables()
+{
     std::stringstream ss;
-    ss << _agentId << ".";
+    ss << *_agentId << ".";
     std::string agentIdString = ss.str();
     for (const Quantifier* quantifier : _engine->getPlanRepository()->getQuantifiers()) {
         for (const Variable* tv : quantifier->getTemplateVariables()) {
-            DomainVariable* dv =
-                    new DomainVariable(makeUniqueId(tv->getName()), agentIdString + tv->getName(), "", tv, _agentId);
+            DomainVariable* dv = new DomainVariable(makeUniqueId(tv->getName()), agentIdString + tv->getName(), "", tv, _agentId);
             _domainVariables.emplace(tv, dv);
         }
     }
 }
 
-const DomainVariable* RobotEngineData::getDomainVariable(const Variable* templateVar) const {
+const DomainVariable* RobotEngineData::getDomainVariable(const Variable* templateVar) const
+{
     auto iterator = _domainVariables.find(templateVar);
     if (iterator != _domainVariables.end()) {
         return iterator->second;
@@ -59,19 +64,21 @@ const DomainVariable* RobotEngineData::getDomainVariable(const Variable* templat
     }
 }
 
-const DomainVariable* RobotEngineData::getDomainVariable(const std::string& sort) const {
+const DomainVariable* RobotEngineData::getDomainVariable(const std::string& sort) const
+{
     const Variable* tv = _engine->getPlanRepository()->getVariables()[Hash64(sort.c_str(), sort.size())];
     return getDomainVariable(tv);
 }
 
-int64_t RobotEngineData::makeUniqueId(const std::string& s) const {
+int64_t RobotEngineData::makeUniqueId(const std::string& s) const
+{
     int64_t ret = (int64_t)(supplementary::AgentIDHash()(_agentId) + std::hash<string>()(s));
-    assert(_engine->getPlanParser()->getParsedElements()->find(ret) ==
-            _engine->getPlanParser()->getParsedElements()->end());
+    assert(_engine->getPlanParser()->getParsedElements()->find(ret) == _engine->getPlanParser()->getParsedElements()->end());
     return ret;
 }
 
-void RobotEngineData::clearSuccessMarks() {
+void RobotEngineData::clearSuccessMarks()
+{
     _successMarks->clear();
 }
 
