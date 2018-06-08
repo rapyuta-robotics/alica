9376fef18c9c07dc282f1fb2420a6bd5fb3e68d9
Gautham Manoharan <gautham.manoharan@rapyuta-robotics.com>
2018-06-08 17:49:44 +0900

    add exception handling to PlanParser (#29)

--- alica_engine/include/engine/containers/PlanTreeInfo.h
+++ alica_engine/include/engine/containers/PlanTreeInfo.h
@@ -3,20 +3,25 @@
 #include <tuple>
 #include <vector>
 
+namespace supplementary
+{
+class AgentID;
+}
+
 namespace alica
 {
 typedef std::tuple<const supplementary::AgentID*, std::vector<int64_t>, std::vector<int64_t>> stdPlanTreeInfo;
 struct PlanTreeInfo
 {
     PlanTreeInfo()
-        : senderID(nullptr)
+            : senderID(nullptr)
     {
     }
 
     PlanTreeInfo(const stdPlanTreeInfo& s)
-        : senderID(std::get<0>(s))
-        , stateIDs(std::get<1>(s))
-        , succeededEPs(std::get<2>(s))
+            : senderID(std::get<0>(s))
+            , stateIDs(std::get<1>(s))
+            , succeededEPs(std::get<2>(s))
     {
     }
 
