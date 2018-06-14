276d442bf771a06f1ce833a953c044cea3f02045
Hendrik <hendrik.skubch@rapyuta-robotics.com>
2018-06-14 14:27:40 +0900

    optimization

--- alica_engine/include/engine/AgentIDConstPtr.h
+++ alica_engine/include/engine/AgentIDConstPtr.h
@@ -4,6 +4,12 @@
 namespace alica
 {
 
+// The current AgentIDManager implementation allows for equality by pointer comparison
+// but this only works as long as there is only one AgentIDManager.
+// Note that the difference in performance is significant, because for the fast check, agent ids do not need to be in cache,
+// whereas for the slow version, any find-in-datastructues operation will likely be stalled several times due to cache misses.
+#define FAST_EQUALITY_CHECK
+
 class AgentIDConstPtr
 {
 public:
@@ -19,8 +25,11 @@ public:
     const supplementary::AgentID* operator->() const { return _ptr; }
 
     operator bool() const { return _ptr != nullptr; }
-
+#ifdef FAST_EQUALITY_CHECK
+    bool operator==(const AgentIDConstPtr o) const { return _ptr == o._ptr; }
+#else
     bool operator==(const AgentIDConstPtr o) const { return _ptr == o._ptr || (_ptr != nullptr && o._ptr != nullptr && *_ptr == *o._ptr); }
+#endif
     bool operator!=(const AgentIDConstPtr id) const { return !AgentIDConstPtr::operator==(id); }
     bool operator<(const AgentIDConstPtr id) const { return *_ptr < *id._ptr; }
     const supplementary::AgentID* get() const { return _ptr; }
