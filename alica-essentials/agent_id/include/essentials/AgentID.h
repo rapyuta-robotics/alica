9ff24a23b9f745f8c2648b0c65409dd68364fd0a
AbhishekS <abhishek.sharma@rapyuta-robotics.com>
2019-05-14 15:56:31 +0900

    Extend AgentId with possibility to be empty

--- alica-essentials/agent_id/include/essentials/AgentID.h
+++ alica-essentials/agent_id/include/essentials/AgentID.h
@@ -10,12 +10,20 @@ class AgentID
     friend struct std::hash<essentials::AgentID>;
 
   public:
+    AgentID()
+        : _type(UUID_TYPE)
+    {
+    }
+    AgentID(const std::vector<uint8_t>& bytes);
     AgentID(const uint8_t* idBytes, int idSize, uint8_t type = UUID_TYPE);
     virtual ~AgentID();
     virtual bool operator==(const AgentID& obj) const;
     virtual bool operator!=(const AgentID& obj) const;
     virtual bool operator<(const AgentID& other) const;
     virtual bool operator>(const AgentID& other) const;
+    void operator=(const std::vector<uint8_t>& other_id);
+    operator bool() const { return !_id.empty(); }
+
     virtual const uint8_t* getRaw() const;
     virtual int getSize() const;
     virtual std::vector<uint8_t> toByteVector() const;
@@ -24,36 +32,35 @@ class AgentID
 
     friend std::ostream& operator<<(std::ostream& os, const essentials::AgentID& obj)
     {
-        if (obj.id.size() <= sizeof(int)) {
+        if (obj._id.size() <= sizeof(int)) {
             std::vector<uint8_t> tmpLong;
-            for (int i = 0; i < static_cast<int>(obj.id.size()); i++) {
-                tmpLong.push_back(obj.id[i]);
+            for (int i = 0; i < static_cast<int>(obj._id.size()); i++) {
+                tmpLong.push_back(obj._id[i]);
             }
-            for (int i = 0; i < static_cast<int>(sizeof(int) - obj.id.size()); i++) {
+            for (int i = 0; i < static_cast<int>(sizeof(int) - obj._id.size()); i++) {
                 tmpLong.push_back(0);
             }
             os << (int)(*tmpLong.data());
         } else {
             std::vector<uint8_t> tmpShort;
-            tmpShort.push_back(obj.id[0]);
-            tmpShort.push_back(obj.id[1]);
+            tmpShort.push_back(obj._id[0]);
+            tmpShort.push_back(obj._id[1]);
             os << (short)(*tmpShort.data()) << "[...]";
             tmpShort.clear();
-            tmpShort.push_back(obj.id[obj.id.size() - 2]);
-            tmpShort.push_back(obj.id[obj.id.size() - 1]);
+            tmpShort.push_back(obj._id[obj._id.size() - 2]);
+            tmpShort.push_back(obj._id[obj._id.size() - 1]);
             os << (short)(*tmpShort.data());
         }
         return os;
     }
-
-    const uint8_t TYPE;
 
     static const uint8_t INT_TYPE = 0;
     static const uint8_t BC_TYPE = 1;
     static const uint8_t UUID_TYPE = 2;
 
   private:
-    std::vector<uint8_t> id;
+    std::vector<uint8_t> _id;
+    uint8_t _type;
 };
 
 struct AgentIDComparator
