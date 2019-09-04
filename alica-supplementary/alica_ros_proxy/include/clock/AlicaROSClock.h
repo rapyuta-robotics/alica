1e9d0086a879419ec3229d633fe84b3112613a1b
Abhishek S <abhishek.sharma@rapyuta-robotics.com>
2019-09-04 18:58:11 +0530

    Removed unnecessary ";"

--- alica-supplementary/alica_ros_proxy/include/clock/AlicaROSClock.h
+++ alica-supplementary/alica_ros_proxy/include/clock/AlicaROSClock.h
@@ -8,8 +8,8 @@ namespace alicaRosProxy
 class AlicaROSClock : public alica::AlicaClock
 {
 public:
-    AlicaROSClock() {};
-    virtual ~AlicaROSClock() {};
+    AlicaROSClock() {}
+    virtual ~AlicaROSClock() {}
     alica::AlicaTime now() const override;
     void sleep(const alica::AlicaTime&) const override;
 };
