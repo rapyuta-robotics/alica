fbec3b55d1f0d409df4c5b55bef9262d27260951
Abhishek S <abhishek.sharma@rapyuta-robotics.com>
2019-09-18 16:04:23 +0530

    Update constructor

--- alica-supplementary/alica_ros_proxy/include/clock/AlicaROSClock.h
+++ alica-supplementary/alica_ros_proxy/include/clock/AlicaROSClock.h
@@ -8,7 +8,7 @@ namespace alicaRosProxy
 class AlicaROSClock : public alica::AlicaClock
 {
 public:
-    AlicaROSClock() {}
+    AlicaROSClock();
     virtual ~AlicaROSClock() {}
     alica::AlicaTime now() const override;
     void sleep(const alica::AlicaTime&) const override;
