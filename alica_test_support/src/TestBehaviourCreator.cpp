8b17806b691dd301d9ee3da7b7b0097e8af59c63
Stephan Opfer <stephan.opfer@rapyuta-robotics.com>
2020-08-13 22:05:46 +0200

    - finished first version of TestBehaviourCreator

--- alica_test_support/src/TestBehaviourCreator.cpp
+++ alica_test_support/src/TestBehaviourCreator.cpp
@@ -1,1 +1,27 @@
 #include "alica/TestBehaviourCreator.h"
+
+namespace alica
+{
+TestBehaviourCreator::TestBehaviourCreator(IBehaviourCreator& defaultBehaviourCreator)
+        : _defaultBehaviourCreator(defaultBehaviourCreator)
+{
+}
+
+std::shared_ptr<BasicBehaviour> TestBehaviourCreator::createBehaviour(int64_t behaviourId)
+{
+    auto behaviourIter = _behaviourMockUps.find(behaviourId);
+    if (behaviourIter == _behaviourMockUps.end()) {
+        return _defaultBehaviourCreator.createBehaviour(behaviourId);
+    } else {
+        return behaviourIter->second;
+    }
+}
+
+void TestBehaviourCreator::setBehaviourMockUp(int64_t behaviourId, std::shared_ptr<BasicBehaviour> behaviourMockUp) {
+    auto insertResultIter = _behaviourMockUps.insert(std::make_pair(behaviourId, behaviourMockUp));
+    if (!insertResultIter.second) {
+        // key already existed, so overwrite its value
+        _behaviourMockUps[behaviourId] = behaviourMockUp;
+    }
+}
+} // namespace alica
