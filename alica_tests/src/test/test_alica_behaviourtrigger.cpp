7b758b0e68d92b4500382f8443a292d3567a3423
Stephan Opfer <stephan.opfer@rapyuta-robotics.com>
2020-09-11 16:16:15 +0200

    - improve launch file, by adding gdb support - fix behaviour trigger test

--- alica_tests/src/test/test_alica_behaviourtrigger.cpp
+++ alica_tests/src/test/test_alica_behaviourtrigger.cpp
@@ -32,18 +32,18 @@ TEST_F(AlicaBehaviourTrigger, triggerTest)
 {
     ASSERT_NO_SIGNAL
     EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1428508297492, 0))->callCounter, 0);
-    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1428508316905, 0))->callCounter, 0);
-    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1428508355209, 0))->callCounter, 0);
-    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1429017274116, 0))->callCounter, 0);
+    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerB>(tc->getBasicBehaviour(1428508316905, 0))->callCounter, 0);
+    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerC>(tc->getBasicBehaviour(1428508355209, 0))->callCounter, 0);
+    EXPECT_EQ(std::dynamic_pointer_cast<alica::NotToTrigger>(tc->getBasicBehaviour(1429017274116, 0))->callCounter, 0);
 
     tc->startEngine();
     alica::AlicaTime duration = alica::AlicaTime::milliseconds(100);
     tc->getAlicaClock().sleep(duration);
 
     EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1428508297492, 0))->callCounter, 0);
-    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1428508316905, 0))->callCounter, 0);
-    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1428508355209, 0))->callCounter, 0);
-    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1429017274116, 0))->callCounter, 0);
+    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerB>(tc->getBasicBehaviour(1428508316905, 0))->callCounter, 0);
+    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerC>(tc->getBasicBehaviour(1428508355209, 0))->callCounter, 0);
+    EXPECT_EQ(std::dynamic_pointer_cast<alica::NotToTrigger>(tc->getBasicBehaviour(1429017274116, 0))->callCounter, 0);
 
     alicaTests::TestWorldModel::getOne()->trigger1->run(true);
     alicaTests::TestWorldModel::getOne()->trigger2->run(true);
@@ -65,9 +65,9 @@ TEST_F(AlicaBehaviourTrigger, triggerTest)
     tc->getAlicaClock().sleep(duration * 2);
 
     EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1428508297492, 0))->callCounter, 3);
-    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1428508316905, 0))->callCounter, 3);
-    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1428508355209, 0))->callCounter, 4);
-    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerA>(tc->getBasicBehaviour(1429017274116, 0))->callCounter, 0);
+    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerB>(tc->getBasicBehaviour(1428508316905, 0))->callCounter, 3);
+    EXPECT_EQ(std::dynamic_pointer_cast<alica::TriggerC>(tc->getBasicBehaviour(1428508355209, 0))->callCounter, 4);
+    EXPECT_EQ(std::dynamic_pointer_cast<alica::NotToTrigger>(tc->getBasicBehaviour(1429017274116, 0))->callCounter, 0);
 }
 }
 }
