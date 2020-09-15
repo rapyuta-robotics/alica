70c4969b8800c1252198e472091fd953c8bd8217
Stephan Opfer <stephan.opfer@rapyuta-robotics.com>
2020-09-15 12:39:01 +0200

    - made SuccessOrFailBehaviour default constructable

--- alica_test_utility/include/alica/test/SuccessOrFailBehaviour.hpp
+++ alica_test_utility/include/alica/test/SuccessOrFailBehaviour.hpp
@@ -8,25 +8,30 @@ template <uint32_t iterationsBeforeTrigger>
 class SuccessOrFailBehaviour : public alica::BasicBehaviour
 {
 public:
-    SuccessOrFailBehaviour<iterationsBeforeTrigger>(bool triggerSuccess, const std::string& nameOfMockedBehaviour);
+    SuccessOrFailBehaviour<iterationsBeforeTrigger>();
     void run(void* msg) override;
+    void setTriggerType(bool triggerSuccess);
 
 protected:
     uint32_t iterationsCounter() const;
     void incIterationsCounter();
+    void initialiseParameters() override;
 
 private:
+    std::mutex _triggerMutex;
+    bool _triggerTypeSet;
     bool _triggerSuccess;
     uint32_t _iterationsBeforeTrigger;
     uint32_t _iterationsCounter;
 };
 
 template <uint32_t iterationsBeforeTrigger>
-SuccessOrFailBehaviour<iterationsBeforeTrigger>::SuccessOrFailBehaviour(bool triggerSuccess, const std::string& nameOfMockedBehaviour)
-        : BasicBehaviour(nameOfMockedBehaviour)
+SuccessOrFailBehaviour<iterationsBeforeTrigger>::SuccessOrFailBehaviour()
+        : BasicBehaviour("SuccessOrFailBehaviour")
         , _iterationsCounter(0)
         , _iterationsBeforeTrigger(iterationsBeforeTrigger)
-        , _triggerSuccess(triggerSuccess)
+        , _triggerTypeSet(false)
+        , _triggerSuccess(false)
 {
 }
 
@@ -40,6 +45,22 @@ template <uint32_t iterationsBeforeTrigger>
 void SuccessOrFailBehaviour<iterationsBeforeTrigger>::incIterationsCounter()
 {
     _iterationsCounter++;
+}
+
+template <uint32_t iterationsBeforeTrigger>
+void SuccessOrFailBehaviour<iterationsBeforeTrigger>::setTriggerType(bool triggerSuccess)
+{
+    std::lock_guard<std::mutex> lockGuard(_triggerMutex);
+    _triggerSuccess = triggerSuccess;
+    _triggerTypeSet = true;
+}
+
+template <uint32_t iterationsBeforeTrigger>
+void SuccessOrFailBehaviour<iterationsBeforeTrigger>::initialiseParameters()
+{
+    // Note: We require the BehaviourMockups to be default constructable, therefore we cannot set the _triggerSuccess flag on construction time.
+    std::lock_guard<std::mutex> lockGuard(_triggerMutex);
+    assert(_triggerTypeSet && "[SuccessOrFailBehavour] Please call setTriggerType(bool triggerSuccess) once before starting the behaviour the first time.");
 }
 
 template <uint32_t iterationsBeforeTrigger>
