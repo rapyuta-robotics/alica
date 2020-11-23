7e8f2307cc4eb635d054303d6114f0615be6a17c
Karasuma1412 <bjoerninorek@gmail.com>
2020-11-23 08:30:50 +0100

    change integer values to unsigned

--- alica_tests/src/test/test_path_parser.cpp
+++ alica_tests/src/test/test_path_parser.cpp
@@ -11,16 +11,16 @@ TEST(PathParserTest, parsePath)
     EXPECT_EQ("Path", params[2]);
     EXPECT_EQ("To", params[3]);
     EXPECT_EQ("Value", params[4]);
-    EXPECT_EQ(5, params.size());
+    EXPECT_EQ(5u, params.size());
 
     params = pathParser.getParams(';', "Alica.Test.Path.To.Value");
     EXPECT_EQ("Alica.Test.Path.To.Value", params[0]);
-    EXPECT_EQ(1, params.size());
+    EXPECT_EQ(1u, params.size());
 
     params = pathParser.getParams(';', "Alica.Test;Path.To.Value");
     EXPECT_EQ("Alica.Test", params[0]);
     EXPECT_EQ("Path.To.Value", params[1]);
-    EXPECT_EQ(2, params.size());
+    EXPECT_EQ(2u, params.size());
 
     params = pathParser.getParams(';', nullptr);
     EXPECT_TRUE(params.empty());
