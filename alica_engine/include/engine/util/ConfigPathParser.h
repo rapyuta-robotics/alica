fd1dadc8390aaa51ee64be42c2a3bb558d1a2691
Karasuma1412 <bjoerninorek@gmail.com>
2020-12-02 11:46:37 +0100

    fix doc

--- alica_engine/include/engine/util/ConfigPathParser.h
+++ alica_engine/include/engine/util/ConfigPathParser.h
@@ -10,7 +10,6 @@ public:
      * Splits the given strings if it finds the given separator.
      * @param separator
      * @param path
-     * @param ap
      * @return The list of strings after everything was splitted.
      */
     std::vector<std::string> getParams(char separator, const std::string& path);
