8bf9a08e307742756d504a1f55b18e56b09f05ad
Karasuma1412 <bjoerninorek@gmail.com>
2020-12-19 23:23:40 +0100

    remove setting _configPath in initConfig

--- alica_engine/src/engine/AlicaContext.cpp
+++ alica_engine/src/engine/AlicaContext.cpp
@@ -97,7 +97,6 @@ YAML::Node AlicaContext::initConfig(const std::string& configPath, const std::st
     YAML::Node node;
     try {
         node = YAML::LoadFile(configPath + agentName + "/Alica.yaml");
-        _configPath = configPath;
     } catch (YAML::BadFile& badFile) {
         AlicaEngine::abort("AC: Could not parse file: ", badFile.msg);
     }
