b48b9869a10ae2ac719fe52c9bc76b24feda4a14
Karasuma1412 <bjoerninorek@gmail.com>
2021-02-09 19:54:49 +0100

    remove implementation of add method from scheduler header

--- alica_engine/include/engine/scheduler/Scheduler.h
+++ alica_engine/include/engine/scheduler/Scheduler.h
@@ -10,16 +10,7 @@ namespace scheduler
     class Scheduler
     {
     public:
-        void add(std::weak_ptr<Job> job)
-        {
-            AlicaClock clock;
-            auto sharedPtr = job.lock();
-            if (sharedPtr) {
-                sharedPtr.get()->scheduledTime = clock.now();
-                queue.push_back(job);
-            }
-
-        };
+        void add(std::weak_ptr<Job> job);
     private:
         std::vector<std::weak_ptr<Job>> queue;
     };
