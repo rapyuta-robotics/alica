0030ba42e21f6f18fb2bffc1bd1bb979e54205df
Karasuma1412 <bjoerninorek@gmail.com>
2021-02-09 19:55:36 +0100

    fix implementation for scheduler add method

--- alica_engine/src/engine/scheduler/Scheduler.cpp
+++ alica_engine/src/engine/scheduler/Scheduler.cpp
@@ -6,9 +6,15 @@ namespace alica
 namespace scheduler
 {
 
-void Scheduler::add(Job job)
+void Scheduler::add(std::weak_ptr<Job> job)
 {
-    std::cerr << adding a job... << std::endl;
+    std::cerr << "adding a job..." << std::endl;
+//    AlicaClock clock;
+//    auto sharedPtr = job.lock();
+//    if (sharedPtr) {
+//        sharedPtr.get()->scheduledTime = clock.now();
+//        queue.push_back(job);
+//    }
 }
 } //namespace scheduler
 } //namespace alica
