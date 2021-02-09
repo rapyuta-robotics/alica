41ce81a719c7a00a7f69f79acd03c58aa99d1183
Karasuma1412 <bjoerninorek@gmail.com>
2021-02-09 19:52:18 +0100

    add constructor and destructor to BasicPlan

--- alica_engine/include/engine/BasicPlan.h
+++ alica_engine/include/engine/BasicPlan.h
@@ -1,10 +1,20 @@
 #pragma once
 
+#include <string>
+
 namespace alica
 {
 class BasicPlan {
+public:
+    BasicPlan(const std::string& name);
+    virtual ~BasicPlan(){};
+
     virtual void init() {};
     virtual void onTermination() {};
+
+    const std::string& getName() const { return _name; }
+private:
+    std::string _name;
 };
 }
 
--- alica_engine/src/engine/BasicPlan.cpp
+++ alica_engine/src/engine/BasicPlan.cpp
@@ -1,1 +1,10 @@
 #include "engine/BasicPlan.h"
+
+namespace alica {
+
+BasicPlan::BasicPlan(const std::string &name)
+        : _name(name)
+{
+}
+
+} //namespace alica
