12e6aca1315685d7e31942ec73e1c737fbaddd5b
Karasuma1412 <bjoerninorek@gmail.com>
2021-02-10 02:18:46 +0100

    remove ConcurrentQueue from test

--- alica_tests/src/test/test_alica_scheduling.cpp
+++ alica_tests/src/test/test_alica_scheduling.cpp
@@ -1,19 +1,7 @@
 #include <gtest/gtest.h>
-#include "engine/containers/ConcurrentQueue.h"
 
 TEST(SchedulingTest, scheduling)
 {
-    /*
-     * Test if moodycamel ConcurrentQueue can be used as intended.
-     * TODO: Replace with an actual test of the new scheduler.
-     */
-    moodycamel::ConcurrentQueue<int> queue;
-    queue.enqueue(25);
 
-    int item;
-    bool found = queue.try_dequeue(item);
-
-    EXPECT_TRUE(found);
-    EXPECT_EQ(item, 25);
 }
 
