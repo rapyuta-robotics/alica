5651cce0bfc2e2c1356197e456a4e31d5cba2332
Karasuma1412 <bjoerninorek@gmail.com>
2021-02-10 04:25:12 +0100

    do not add jobs to queue that are already queued

--- alica_engine/src/engine/scheduler/Scheduler.cpp
+++ alica_engine/src/engine/scheduler/Scheduler.cpp
@@ -13,10 +13,20 @@ Scheduler::Scheduler(int numberOfThreads)
 
 void Scheduler::add(std::shared_ptr<Job> job)
 {
+    // check if job is already in queue
+    auto it = std::find_if(queue.begin(), queue.end(), [&](std::shared_ptr<Job> const& queuedJob) {
+       return *queuedJob == *(job.get());
+    });
+
+    if (it == queue.end()) {
+        // do not add job to queue if already queued
+        return;
+    }
+
     std::cerr << "adding a job..." << std::endl;
     for (auto job : job.get()->prerequisites) {
         if (auto jobSharedPtr = job.lock())  {
-            queue.push_back(jobSharedPtr);
+            add(jobSharedPtr);
         }
     }
     queue.push_back(job);
