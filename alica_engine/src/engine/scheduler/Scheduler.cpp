3edeec00d0b97e358746717fd4e977e797a30b46
Karasuma1412 <bjoerninorek@gmail.com>
2021-02-10 04:07:52 +0100

    add jobs and prerequisites to queue

--- alica_engine/src/engine/scheduler/Scheduler.cpp
+++ alica_engine/src/engine/scheduler/Scheduler.cpp
@@ -6,19 +6,20 @@ namespace alica
 namespace scheduler
 {
 
-Scheduler::Scheduler(int numberOfThreads) {
-
+Scheduler::Scheduler(int numberOfThreads)
+{
+    std::cerr << "Scheduler Threads: " << numberOfThreads << std::endl;
 }
 
 void Scheduler::add(std::shared_ptr<Job> job)
 {
     std::cerr << "adding a job..." << std::endl;
-//    AlicaClock clock;
-//    auto sharedPtr = job.lock();
-//    if (sharedPtr) {
-//        sharedPtr.get()->scheduledTime = clock.now();
-//        queue.push_back(job);
-//    }
+    for (auto job : job.get()->prerequisites) {
+        if (auto jobSharedPtr = job.lock())  {
+            queue.push_back(jobSharedPtr);
+        }
+    }
+    queue.push_back(job);
 }
 } //namespace scheduler
 } //namespace alica
