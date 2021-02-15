7aa939632ba4e22e643ca62f64e4d1b202a7de4c
Karasuma1412 <bjoerninorek@gmail.com>
2021-02-15 11:44:33 +0100

    implement threadpool, synchronised queue access

--- alica_engine/src/engine/scheduler/Scheduler.cpp
+++ alica_engine/src/engine/scheduler/Scheduler.cpp
@@ -8,7 +8,24 @@ namespace scheduler
 
 Scheduler::Scheduler(int numberOfThreads)
 {
-    std::cerr << "Scheduler Threads: " << numberOfThreads << std::endl;
+    for (int i = 0; i < numberOfThreads; i++) {
+        _workers.push_back(new std::thread(&Scheduler::workerFunction, this));
+    }
+}
+
+Scheduler::~Scheduler()
+{
+    running = false;
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        queue.clear();
+    }
+
+    for (auto worker : _workers) {
+        condition.notify_all();
+        worker->join();
+        delete worker;
+    }
 }
 
 void Scheduler::add(std::shared_ptr<Job> job)
@@ -29,7 +46,50 @@ void Scheduler::add(std::shared_ptr<Job> job)
             add(jobSharedPtr);
         }
     }
-    queue.push_back(job);
+    {
+        std::unique_lock<std::mutex> lock(mtx);
+        queue.push_back(job);
+    }
+
+    condition.notify_one();
+}
+
+void Scheduler::workerFunction()
+{
+    while(running) {
+        Job *job;
+        std::shared_ptr<Job> jobSharedPtr;
+        bool executeJob = true;
+
+        {
+            std::unique_lock<std::mutex> lock(mtx);
+            // wait when queue is empty. Do no wait when queue has jobs or the schedulers destructor is called.
+            condition.wait(lock, [this]{return !queue.empty() || !running;});
+
+            if (queue.empty() || !running) {
+                continue;
+            }
+
+            jobSharedPtr = queue.front();
+            job = jobSharedPtr.get();
+            queue.erase(queue.begin());
+
+            for (std::weak_ptr<Job> j : job->prerequisites) {
+                if (j.lock()) {
+                    // Prerequisite jobs not finished, do not execute job
+                    executeJob = false;
+                    break;
+                }
+            }
+        }
+
+        if (executeJob) {
+            job->cb();
+        } else {
+            add(jobSharedPtr);
+        }
+
+    }
 }
 } //namespace scheduler
 } //namespace alica
