19873d8528ecc5b4078d19ad94a20ce18b128476
Karasuma1412 <bjoerninorek@gmail.com>
2021-02-15 11:42:18 +0100

    add variables for synchronisation, add destructor

--- alica_engine/include/engine/scheduler/Scheduler.h
+++ alica_engine/include/engine/scheduler/Scheduler.h
@@ -2,6 +2,9 @@
 
 #include "engine/scheduler/Job.h"
 #include <vector>
+#include <condition_variable>
+#include <mutex>
+#include <thread>
 
 namespace alica
 {
@@ -11,9 +14,16 @@ namespace scheduler
     {
     public:
         Scheduler(int numberOfThreads);
+        ~Scheduler();
         void add(std::shared_ptr<Job> job);
     private:
         std::vector<std::shared_ptr<Job>> queue;
+        std::condition_variable condition;
+        std::vector<std::thread*> _workers;
+        std::mutex mtx;
+        bool running = true;
+
+        void workerFunction();
     };
 }
 }
