035b8605e63f5de418cbba3bf151242f259ae398
Karasuma1412 <bjoerninorek@gmail.com>
2021-03-22 19:03:41 +0100

    remove mutex, insert without sorting queue

--- alica_engine/src/engine/scheduler/JobQueue.cpp
+++ alica_engine/src/engine/scheduler/JobQueue.cpp
@@ -4,18 +4,12 @@ namespace alica {
 namespace scheduler {
     void JobQueue::insert(std::shared_ptr<Job>&& job)
     {
-        {
-            std::unique_lock<std::mutex> lock(_mtx);
-            _queue.insert(std::upper_bound(_queue.begin(), _queue.end(), job), std::move(job));
-        }
+        _queue.insert(std::upper_bound(_queue.begin(), _queue.end(), job), std::move(job));
     }
 
     bool JobQueue::isEmpty()
     {
-        {
-            std::unique_lock<std::mutex> lock(_mtx);
-            return _queue.empty();
-        }
+        return _queue.empty();
     }
 
     std::shared_ptr<Job> JobQueue::popNext()
@@ -24,20 +18,14 @@ namespace scheduler {
             return nullptr;
         }
 
-        {
-            std::unique_lock<std::mutex> lock(_mtx);
-            std::shared_ptr<Job> job = _queue.front();
-            _queue.erase(_queue.begin());
-            return job;
-        }
+        std::shared_ptr<Job> job = _queue.front();
+        _queue.erase(_queue.begin());
+        return job;
     }
 
     void JobQueue::clear()
     {
-        {
-            std::unique_lock<std::mutex> lock(_mtx);
-            _queue.clear();
-        }
+        _queue.clear();
     }
 }
 }
