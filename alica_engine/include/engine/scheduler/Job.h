328eebdceddf5be289a01314025877ee17893802
Karasuma1412 <bjoern.schroder@rapyuta-robotics.com>
2021-08-12 10:41:58 +0200

    remove unused members of job

--- alica_engine/include/engine/scheduler/Job.h
+++ alica_engine/include/engine/scheduler/Job.h
@@ -12,18 +12,12 @@ namespace scheduler
 {
 struct Job
 {
-    int id;
     std::function<void()> cb;
-    bool isRepeated;
     AlicaTime repeatInterval;
 
-    Job(int id, std::function<void()> cb)
+    Job(std::function<void()> cb)
             : cb(cb)
-            , id(id)
-            , isRepeated(false)
     {}
-
-    bool operator==(const Job& r) const { return id == r.id; }
 };
 } // namespace scheduler
 } // namespace alica
