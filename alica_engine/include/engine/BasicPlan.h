d6b7ab8d57e206f869f13544d13add84f51b3eca
Karasuma1412 <bjoern.schroder@rapyuta-robotics.com>
2021-08-17 22:55:48 +0200

    add context and config

--- alica_engine/include/engine/BasicPlan.h
+++ alica_engine/include/engine/BasicPlan.h
@@ -1,10 +1,15 @@
 #pragma once
 
+#include "engine/PlanInterface.h"
+
 #include <string>
+#include <atomic>
 
 namespace alica
 {
 
+class RunningPlan;
+class Configuration;
 class AlicaEngine;
 
 class BasicPlan
@@ -15,10 +20,34 @@ public:
 
     virtual void init(){};
     virtual void onTermination(){};
+
+    void doInit()
+    {
+        init();
+        _planStarted = true;
+    }
+
+    void doTerminate()
+    {
+        onTermination();
+        _planStarted = false;
+    }
+
     void start();
     void stop();
     void setEngine(AlicaEngine* engine) { _ae = engine; }
+
+    ThreadSafePlanInterface getPlanContext() { return ThreadSafePlanInterface(isPlanStarted() ? _context : nullptr); }
+    void setRunningPlan(RunningPlan* rp) { _context = rp; }
+
+    bool isPlanStarted() const { return _planStarted; }
+
+    void setConfiguration(const Configuration* conf);
+
 private:
     alica::AlicaEngine* _ae;
+    RunningPlan* _context;
+    std::atomic<bool> _planStarted;
+    const Configuration* _configuration;
 };
 } // namespace alica
