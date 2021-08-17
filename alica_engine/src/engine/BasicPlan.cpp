d41aa25c644d1ecd6824a607db7cadb30e832ea6
Karasuma1412 <bjoern.schroder@rapyuta-robotics.com>
2021-08-17 22:57:08 +0200

    update job creation, add setConfiguration

--- alica_engine/src/engine/BasicPlan.cpp
+++ alica_engine/src/engine/BasicPlan.cpp
@@ -1,21 +1,33 @@
 #include "engine/BasicPlan.h"
+
 #include "engine/AlicaEngine.h"
+#include "engine/model/Configuration.h"
 
 namespace alica
 {
 
-BasicPlan::BasicPlan() {}
+BasicPlan::BasicPlan()
+        : _planStarted(false)
+        , _context(nullptr)
+        , _configuration(nullptr)
+{
+}
 
 void BasicPlan::start()
 {
-    std::function<void()> cb = std::bind(&BasicPlan::init, this);
+    std::function<void()> cb = std::bind(&BasicPlan::doInit, this);
     _ae->editScheduler().schedule(cb);
 }
 
 void BasicPlan::stop()
 {
-    std::function<void()> cb = std::bind(&BasicPlan::onTermination, this);
+    std::function<void()> cb = std::bind(&BasicPlan::doTerminate, this);
     _ae->editScheduler().schedule(cb);
+}
+
+void BasicPlan::setConfiguration(const Configuration* conf)
+{
+    _configuration = conf;
 }
 
 } // namespace alica
