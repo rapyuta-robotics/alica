340123cde0b6fa5ef7145f4c48099ec4e7385db8
bjoernschroeder <bschroederprogramming@gmail.com>
2021-10-11 14:11:17 +0200

    remove member variable and constructors

--- alica_engine/include/engine/IAlicaTrace.h
+++ alica_engine/include/engine/IAlicaTrace.h
@@ -10,18 +10,10 @@ class IAlicaTrace
 {
 public:
     virtual ~IAlicaTrace() = default;
-    IAlicaTrace() = default;
-    IAlicaTrace(const std::string& opName, std::optional<const std::string> parent = std::nullopt);
-    IAlicaTrace(IAlicaTrace&& other) = default;
-    IAlicaTrace& operator=(IAlicaTrace&& other) = default;
-
     virtual void setTag(const std::string& key, const std::string& value) = 0;
     virtual void setLog(std::pair<std::string, std::string> fields) = 0;
     virtual void markError(const std::string& description) = 0;
     virtual std::string context() const = 0;
-
-private:
-    std::unique_ptr<IAlicaTrace> _rawTrace;
 };
 
 class IAlicaTraceFactory
