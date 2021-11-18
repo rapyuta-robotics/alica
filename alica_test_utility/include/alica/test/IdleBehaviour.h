b938f62977a8d1a5102d13ac531f78041396041c
bjoernschroeder <bschroederprogramming@gmail.com>
2021-11-18 01:30:38 +0100

    update test_utility

--- alica_test_utility/include/alica/test/IdleBehaviour.h
+++ alica_test_utility/include/alica/test/IdleBehaviour.h
@@ -2,13 +2,18 @@
 
 #include <engine/BasicBehaviour.h>
 
+namespace alica
+{
+    class IAlicaWorldModel;
+}
+
 namespace alica::test
 {
 class IdleBehaviour : public BasicBehaviour
 {
 public:
-    explicit IdleBehaviour()
-            : BasicBehaviour("IdleBehaviour"){};
+    explicit IdleBehaviour(alica::IAlicaWorldModel* wm)
+            : BasicBehaviour("IdleBehaviour", wm){};
     void run(void* msg) override;
 };
 } // namespace alica::mockups
