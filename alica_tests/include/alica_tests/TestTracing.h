8a827b2fad367f4f981beb198ecf2565b32ca8c8
bjoernschroeder <bschroederprogramming@gmail.com>
2021-11-24 02:54:52 +0100

    fix TestTracing

--- alica_tests/include/alica_tests/TestTracing.h
+++ alica_tests/include/alica_tests/TestTracing.h
@@ -4,18 +4,39 @@
 #include <string>
 #include <unordered_map>
 #include <optional>
+#include <cassert>
 
 namespace alicaTestTracing
 {
 class AlicaTestTrace : public alica::IAlicaTrace
 {
 public:
-    AlicaTestTrace(const std::string& opName, std::optional<const std::string> parent);
+    AlicaTestTrace(const std::string& opName, std::optional<const std::string> parent)
+        : _opName(opName)
+    {
+        if (parent) {
+            _parent = *parent;
+        }
+    }
+    ~AlicaTestTrace() {};
 
-    void setTag(const std::string& key, const std::string& value);
-    void setLog(std::pair<std::string, std::string> logEntry);
-    void markError(const std::string& description);
-    std::string context() const;
+    void setTag(const std::string& key, const std::string& value)
+    {
+        alicaTests::TestWorldModel::getOne()->tracingTags.push_back({key, value});
+    }
+    void setLog(std::pair<std::string, std::string> logEntry)
+    {
+        alicaTests::TestWorldModel::getOne()->tracingLogs.push_back({logEntry.first, logEntry.second});
+    }
+    void markError(const std::string& description)
+    {
+        alicaTests::TestWorldModel::getOne()->tracingTags.push_back({"error", "true"});
+        alicaTests::TestWorldModel::getOne()->tracingTags.push_back({"error.description", description});
+    }
+    std::string context() const
+    {
+        return _opName;
+    }
 
     std::string _opName;
     std::string _parent;
@@ -24,9 +45,14 @@ public:
 class AlicaTestTraceFactory : public alica::IAlicaTraceFactory
 {
 public:
-    AlicaTestTraceFactory() {};
+    AlicaTestTraceFactory(){};
     ~AlicaTestTraceFactory() = default;
 
-    std::unique_ptr<alica::IAlicaTrace> create(const std::string& opName, std::optional<const std::string> parent) const;
+    std::unique_ptr<alica::IAlicaTrace> create(const std::string& opName, std::optional<const std::string> parent = std::nullopt) const
+    {
+        assert(!opName.empty());
+        std::unique_ptr<AlicaTestTrace> trace = std::make_unique<AlicaTestTrace>(opName, parent);
+        return trace;
+    }
 };
 } // namespace alicaDummyTracing
