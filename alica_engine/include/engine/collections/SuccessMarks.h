c7ec7f186114d206e20e6cb6d023cbeffd54c035
Veeraj S Khokale <veeraj.khokale@rapyuta-robotics.com>
2021-11-30 20:31:25 +0530

    Temp

--- alica_engine/include/engine/collections/SuccessMarks.h
+++ alica_engine/include/engine/collections/SuccessMarks.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "engine/Types.h"
 
-#include <unordered_set>
+#include <vector>
+#include <utility>
 
 namespace alica
 {
@@ -17,20 +18,19 @@ public:
     SuccessMarks();
     ~SuccessMarks();
 
-    void limitToPlans(const AbstractPlanGrp& active);
-    void update(const AlicaEngine* ae, const std::vector<std::size_t>& succeededContexts);
+    void limitToContexts(const std::vector<std::pair<std::size_t, const AbstractPlan*>>& activeContexts);
+    void fromMsg(const AlicaEngine* ae, const std::vector<std::size_t>& msg);
 
     void clear();
     EntryPointGrp succeededEntryPoints(std::size_t parentContextHash, const AbstractPlan* p) const;
     void removePlan(std::size_t parentContextHash, const AbstractPlan* plan);
     void markSuccessful(std::size_t parentContextHash, const EntryPoint* e);
 
-    bool succeeded(const AbstractPlan* p, const EntryPoint* e) const;
-    bool anyTaskSucceeded(const AbstractPlan* p) const;
-    std::vector<std::size_t> toContextGrp() const;
+    bool succeeded(std::size_t parentContextHash, const EntryPoint* e) const;
+    bool anyTaskSucceeded(std::size_t parentContextHash, const AbstractPlan* p) const;
+    std::vector<std::size_t> toMsg() const;
 
 private:
-    std::unordered_set<std::size_t> _successMarks;
 };
 
 } /* namespace alica */
