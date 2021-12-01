d7a173cb5e9bf9eb745aed4dbefc00af5989c15c
christianhelm <christian.helm@rapyuta-robotics.com>
2021-12-01 10:39:08 +0900

    Fix hash_combine

--- alica_engine/include/engine/util/HashFunctions.h
+++ alica_engine/include/engine/util/HashFunctions.h
@@ -20,9 +20,9 @@ inline void hash_combine(std::size_t& seed, const T& value)
     // https://github.com/HowardHinnant/hash_append/issues/7
     // Not sure we ever need 32bit, but here it is...
     if constexpr (sizeof(std::size_t) == 4) {
-        seed ^= std::hash<T>{}(value) + 0x9e3779b9U + (seed << 6) + (seed >> 2);
+        seed ^= CityHash32(reinterpret_cast<const char*>(&value), sizeof(size_t)) + 0x9e3779b9U + (seed << 6) + (seed >> 2);
     } else if constexpr (sizeof(std::size_t) == 8) {
-        seed ^= std::hash<T>{}(value) + 0x9e3779b97f4a7c15LLU + (seed << 12) + (seed >> 4);
+        seed ^= CityHash64(reinterpret_cast<const char*>(&value), sizeof(size_t)) + 0x9e3779b97f4a7c15LLU + (seed << 12) + (seed >> 4);
     } else {
         static_assert(dependent_false<T>::value, "hash_combine not implemented");
     }
