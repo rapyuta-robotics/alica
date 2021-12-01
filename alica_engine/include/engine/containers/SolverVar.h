56497fe4e81cc8c40cecad7135a2ba959090a72e
Athish <athish.thirumal@rapyuta-robotics.com>
2021-12-02 03:19:38 +0530

    use std::variant

--- alica_engine/include/engine/containers/SolverVar.h
+++ alica_engine/include/engine/containers/SolverVar.h
@@ -9,7 +9,7 @@ namespace alica
 struct SolverVar
 {
     int64_t id;
-    uint8_t value[Variant::kVariantSize];
+    uint8_t value[variant::kVariantSize];
 };
 
 } /* namespace alica */
--- alica_engine/src/engine/collections/Variant.cpp
+++ alica_engine/src/engine/collections/Variant.cpp
@@ -3,7 +3,50 @@
 namespace alica
 {
 
-constexpr size_t Variant::kUnionSize;
-constexpr size_t Variant::kVariantSize;
+int variant::serializeTo(uint8_t* arr, const Variant& var)
+{
+    arr[0] = static_cast<uint8_t>(var.index());  // -1 to correct to Type
+    std::visit( [arr](auto&& arg) {
+        using T = std::decay_t<decltype(arg)>;
+        if constexpr (!std::is_same_v<std::monostate, T>) {
+            memcpy(arr + 1, &arg, kUnionSize);
+        }
+    }, var );
+    return static_cast<int>(kVariantSize);
+}
+
+int variant::loadFrom(const uint8_t* arr, Variant& var)
+{
+    auto type = static_cast<Type>(arr[0]);
+    if (type == 1) {
+        double data;
+        memcpy(&data, arr + 1, kUnionSize);
+        var = data;
+    }
+    else if (type == 2) {
+        float data;
+        memcpy(&data, arr + 1, kUnionSize);
+        var = data;
+    }
+    else if (type == 3) {
+        bool data;
+        memcpy(&data, arr + 1, kUnionSize);
+        var = data;
+    }
+    else if (type == 4) {
+        void* data;
+        memcpy(data, arr + 1, kUnionSize);
+        var = data;
+    }
+    else if (type == 5) {
+        int64_t data;
+        memcpy(&data, arr + 1, kUnionSize);
+        var = data;
+    }
+    else {
+        var.emplace<std::monostate>();
+    }
+    return static_cast<int>(kVariantSize);
+}
 
 } // namespace alica
