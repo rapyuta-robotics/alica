e72d30af8c93ee51b3fbb33d74b5537adc73722b
bjoernschroeder <39679198+bjoernschroeder@users.noreply.github.com>
2021-12-07 14:06:28 +0100

    Bas tracing test + TestTracing implementation (#177)
    * implement TestTracing
    
    * add fixture for testTracing
    
    * create plan for testing, regenerate
    
    * fixes after rebase
    
    * use size_t for vector size comparison
    
    * regenerate files
    
    Co-authored-by: veerajsk <54059004+veerajsk@users.noreply.github.com>

---
+++ alica_tests/autogenerated/src/constraints/TestTracingMasterPlan691392966514374878Constraints.cpp
@@ -0,0 +1,21 @@
+#include "constraints/TestTracingMasterPlan691392966514374878Constraints.h"
+/*PROTECTED REGION ID(ch691392966514374878) ENABLED START*/
+// Add additional options here
+/*PROTECTED REGION END*/
+
+namespace alica
+{
+// Plan:TestTracingMasterPlan
+/*
+ * Tasks:
+ * - EP:4059536593953041663 : DefaultTask (1225112227903)
+ *
+ * States:
+ * - 2832176823961443072 (2832176823961443072)
+ * - 3077460522716760463 (3077460522716760463)
+ *
+ * Vars:
+ */
+// State: 2832176823961443072
+// State: 3077460522716760463
+} // namespace alica
