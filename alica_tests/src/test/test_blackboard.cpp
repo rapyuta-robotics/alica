8db4b7aef4b020e1c7e447c1435b2b543048b1fe
bjoernschroeder <bschroederprogramming@gmail.com>
2021-12-15 08:46:15 +0100

    implement unittests

--- alica_tests/src/test/test_blackboard.cpp
+++ alica_tests/src/test/test_blackboard.cpp
@@ -1,4 +1,6 @@
 #include "test_alica.h"
+#include "alica_tests/TestWorldModel.h"
+#include <alica/test/Util.h>
 
 namespace alica
 {
@@ -13,11 +15,23 @@ protected:
     bool stepEngine() const override { return false; }
 };
 
-TEST_F(TestBlackBoard, testRegisterValue)
+TEST_F(TestBlackBoard, testParameterPassing)
 {
-    alica::AlicaTime sleepTime = alica::AlicaTime::seconds(1);
     ae->start();
-    ae->getAlicaClock().sleep(sleepTime * 1);
+    ae->getAlicaClock().sleep(alica::AlicaTime::milliseconds(100));
+    auto wm = dynamic_cast<alicaTests::TestWorldModel*>(ae->getWorldModel());
+    EXPECT_EQ(wm->passedParameters["behaviourParameter"], 1);
+    EXPECT_EQ(wm->passedParameters["planParameter"], 2);
+}
+
+TEST_F(TestBlackBoard, testRequiresParameters) 
+{
+    ae->start();
+    auto wm = dynamic_cast<alicaTests::TestWorldModel*>(ae->getWorldModel());
+    // Behaviour has requiresParameters set to true
+    EXPECT_TRUE(alica::test::Util::getBasicBehaviour(ae, 831400441334251602, 0)->getRequiresParameters());
+    // SubPlan has requiresParameters set to true
+    EXPECT_TRUE(alica::test::Util::getBasicPlan(ae, 1692837668719979457, 0)->getRequiresParameters());
 }
 }
 } // namespace alica
