3827f9f3c74badcb8c87d2ac207ff09541225308
bjoernschroeder <bschroederprogramming@gmail.com>
2022-02-23 13:46:38 +0100

    remove cancelAccepted, use has_value() with optionals

--- supplementary/alica_templates/Expr/src/DummyImplementation4126421719858579722.cpp
+++ supplementary/alica_templates/Expr/src/DummyImplementation4126421719858579722.cpp
@@ -58,7 +58,7 @@ bool PreCondition3469760593538210700::evaluate(std::shared_ptr<RunningPlan> rp, 
 {
     /*PROTECTED REGION ID(293344705861516112) ENABLED START*/
     LockedBlackboardRO bb = LockedBlackboardRO(*(rp->getParent()->getBasicPlan()->getBlackboard()));
-    return bb.get<std::optional<bool>>("cancelAccepted") == std::nullopt && bb.get<std::optional<std::vector<int32_t>>>("result") != std::nullopt;
+    return bb.get<std::optional<std::vector<int32_t>>>("result").has_value();
     /*PROTECTED REGION END*/
 }
 
@@ -79,8 +79,8 @@ bool PreCondition3469760593538210700::evaluate(std::shared_ptr<RunningPlan> rp, 
 bool PreCondition2084505765197602547::evaluate(std::shared_ptr<RunningPlan> rp, const IAlicaWorldModel* wm)
 {
     /*PROTECTED REGION ID(161866564931915050) ENABLED START*/
-    LockedBlackboardRO bb = LockedBlackboardRO(*(rp->getParent()->getBasicPlan()->getBlackboard()));
-    return bb.get<std::optional<bool>>("cancelAccepted") == true;
+    // Add failure condition if necessary
+    return false;
     /*PROTECTED REGION END*/
 }
 
@@ -92,17 +92,15 @@ void DummyImplementation4126421719858579722::run(void* msg)
     if (!context.isValid()) {
         return;
     }
+
     LockedBlackboardRW bb = LockedBlackboardRW(*(context.getRunningPlan()->getParent()->getBasicPlan()->getBlackboard()));
-    if (bb.get<std::optional<int32_t>>("goal") != std::nullopt) {
+
+    if (bb.get<std::optional<int32_t>>("goal").has_value()) {
         _currentGoal = bb.get<std::optional<int32_t>>("goal").value();
         bb.get<std::optional<int32_t>>("goal") = std::nullopt;
-    } else if (bb.get<std::optional<bool>>("cancel") == true) {
-        auto result = std::vector<int32_t>();
-        result.push_back(-1);
-        bb.get<std::optional<std::vector<int32_t>>>("result") = result;
-        bb.get<std::optional<bool>>("cancel") = std::nullopt;
-        bb.get<std::optional<bool>>("cancelAccepted") = true;
-    } else {
+    }
+
+    if (!bb.get<std::optional<int32_t>>("result").has_value()) {
         auto result = std::vector<int32_t>();
         result.push_back(14);
         bb.get<std::optional<std::vector<int32_t>>>("result") = result;
