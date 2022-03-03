7ca145e25647a3a65a6e624c0ab8786d15198cf3
Athish T <athish.thirumal@rapyuta-robotics.com>
2022-03-03 12:51:02 +0530

    compose changes

---
+++ supplementary/alica_designer_runtime/run_designer.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+DIR=$(dirname $(realpath $0))
+
+action=${1:-start}
+
+set -a
+source $HOME/.bashrc
+source $DIR/config.env
+set +a
+
+if [ $action == "start" ]; then
+
+	if [ $NATIVE_MODE == "true" ]; then
+		uid=$(id -u) gid=$(id -g) docker-compose -f docker-compose.yml -f docker-compose.native_mode.yml up
+	else
+		uid=$(id -u) gid=$(id -g) docker-compose up
+	fi
+
+elif [ $action == "reset" ]; then
+	docker-compose down -v
+else
+	echo "Usage: run_designer.sh [start/reset]"
+fi
