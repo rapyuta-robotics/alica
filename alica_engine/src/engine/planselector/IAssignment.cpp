2d770937751110e6e6aa093f515748634469fa76
bjoernschroeder <bschroederprogramming@gmail.com>
2022-03-23 04:22:37 +0100

    use signed int for dynamicEpId

--- alica_engine/src/engine/planselector/IAssignment.cpp
+++ alica_engine/src/engine/planselector/IAssignment.cpp
@@ -54,7 +54,7 @@ PartialAssignmentView IAssignment::getAgentsWorking(const EntryPoint* ep) const
     return PartialAssignmentView(ep->getIndex(), _impl);
 }
 
-PartialAssignmentView IAssignment::getAgentsWorking(int64_t epid, uint64_t dynEpId = 0) const
+PartialAssignmentView IAssignment::getAgentsWorking(int64_t epid, int64_t dynEpId = 0) const
 {
     const EntryPointGrp& eps = _impl->getPlan()->getEntryPoints();
     for (int i = 0; i < static_cast<int>(eps.size()); ++i) {
