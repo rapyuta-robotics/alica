52bca2345608d19dfafa2d0a276e6064bb29b640
bjoernschroeder <bjoernschroder@rapyuta-robotics.com>
2022-06-07 03:23:31 +0200

    regenerate files

--- supplementary/alica_ros_turtlesim/alica/Expr/include/alica/conditions/conditions.h
+++ supplementary/alica_ros_turtlesim/alica/Expr/include/alica/conditions/conditions.h
@@ -6,6 +6,7 @@ class Blackboard;
 class RunningPlan;
 class IAlicaWorldModel;
 
-bool conditionMove2Init748720375848597116(const Blackboard* input, const RunningPlan* rp, const IAlicaWorldModel* wm);
+bool conditionDefaultCondition2190266318562141841(const Blackboard* input, const RunningPlan* rp, const IAlicaWorldModel* wm);
 bool conditionInit2Move974606107671315045(const Blackboard* input, const RunningPlan* rp, const IAlicaWorldModel* wm);
+bool conditionMove2Init748720375848597116(const Blackboard* input, const RunningPlan* rp, const IAlicaWorldModel* wm);
 } /* namespace alica */
--- supplementary/alica_ros_turtlesim/alica/Expr/src/conditions/conditions.cpp
+++ supplementary/alica_ros_turtlesim/alica/Expr/src/conditions/conditions.cpp
@@ -3,20 +3,31 @@
 #include <engine/IAlicaWorldModel.h>
 #include <engine/RunningPlan.h>
 #include <engine/blackboard/Blackboard.h>
-#include <alica_ros_turtlesim/world_model.hpp>
+#include <iostream>
+
+/*PROTECTED REGION ID(conditionSource) ENABLED START*/
+// Add additional options here
+#include "aliica_ros_turtlesim/world_model.hpp"
+/*PROTECTED REGION END*/
 
 namespace alica
 {
-bool conditionMove2Init748720375848597116(const Blackboard* input, const RunningPlan* rp, const IAlicaWorldModel* wm)
+bool conditionDefaultCondition2190266318562141841(const Blackboard* input, const RunningPlan* rp, const IAlicaWorldModel* wm)
 {
-    /*PROTECTED REGION ID(condition1136497454350831106) ENABLED START*/
-    return turtlesim::ALICATurtleWorldModel::get()->getInit();
+    /*PROTECTED REGION ID(condition2190266318562141841) ENABLED START*/
+    return false;
     /*PROTECTED REGION END*/
 }
 bool conditionInit2Move974606107671315045(const Blackboard* input, const RunningPlan* rp, const IAlicaWorldModel* wm)
 {
-    /*PROTECTED REGION ID(condition1597434482701133956) ENABLED START*/
+    /*PROTECTED REGION ID(condition974606107671315045) ENABLED START*/
     return rp->isAnyChildStatus(PlanStatus::Success);
+    /*PROTECTED REGION END*/
+}
+bool conditionMove2Init748720375848597116(const Blackboard* input, const RunningPlan* rp, const IAlicaWorldModel* wm)
+{
+    /*PROTECTED REGION ID(condition748720375848597116) ENABLED START*/
+    return turtlesim::ALICATurtleWorldModel::get()->getInit();
     /*PROTECTED REGION END*/
 }
 } /* namespace alica */
