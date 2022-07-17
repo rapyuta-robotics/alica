7842364d2d373c338e31dbab398aa22e6b999a5f
¨triccyx¨ <triccyx@gmail.com>
2022-07-17 17:02:15 +0200

    fix supplementary tests

--- alica_tests/src/ConstraintTestPlanDummySolver.cpp
+++ alica_tests/src/ConstraintTestPlanDummySolver.cpp
@@ -22,8 +22,8 @@ namespace reasoner
 int ConstraintTestPlanDummySolver::s_existsSolutionCallCounter = 0;
 int ConstraintTestPlanDummySolver::s_getSolutionCallCounter = 0;
 
-ConstraintTestPlanDummySolver::ConstraintTestPlanDummySolver(AlicaEngine* ae)
-        : ISolver(ae)
+ConstraintTestPlanDummySolver::ConstraintTestPlanDummySolver(Blackboard& blackboard, const VariableSyncModule& resultStore, const YAML::Node& config)
+        : ISolver(blackboard, resultStore, config)
 {
 }
 
@@ -39,7 +39,7 @@ bool ConstraintTestPlanDummySolver::existsSolutionImpl(SolverContext*, const std
 
 bool ConstraintTestPlanDummySolver::getSolutionImpl(SolverContext* ctx, const std::vector<shared_ptr<ProblemDescriptor>>& calls, std::vector<int64_t>& results)
 {
-    Blackboard& bb = getAlicaEngine()->editBlackboard();
+    Blackboard& bb = editBlackboard();
     SimpleContext<SolverVariable>* tdc = static_cast<SimpleContext<SolverVariable>*>(ctx);
     for (const auto& var : tdc->getVariables()) {
         std::string s = std::to_string(var->getId());
