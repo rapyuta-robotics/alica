8972e0a8a0d1d120487730710ba30ef94a959197
veerajsk <54059004+veerajsk@users.noreply.github.com>
2022-08-18 19:22:39 +0530

    Setup tracing after blackboard (#314)
    * Setup tracing after blackboard
    
    Setup the blackboard before tracing so that blackboard values can be
    used in settup up custom tracing
    
    * Fix flaky test

--- alica_engine/src/engine/RunnableObject.cpp
+++ alica_engine/src/engine/RunnableObject.cpp
@@ -40,8 +40,8 @@ void RunnableObject::stop()
 
     stopRunCalls();
     doTerminate();
-    cleanupBlackboard();
     _runnableObjectTracer.cleanupTraceContext();
+    cleanupBlackboard();
 
     _started = false;
 }
@@ -55,9 +55,10 @@ void RunnableObject::start(RunningPlan* rp)
 
     _runningplanContext = rp;
 
+    setupBlackboard();
+    // setup the trace context after setting up the blackboard so that the blackboard can be used for setting up custom tracing
     // TODO cleanup: pass trace factory in constructor. can't do now as _engine isn't available
     _runnableObjectTracer.setupTraceContext(_name, _runningplanContext, _engine->getTraceFactory());
-    setupBlackboard();
     doInit();
     scheduleRunCalls();
 }
