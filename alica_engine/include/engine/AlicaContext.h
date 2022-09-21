bdf0c024adffd7ad2be2baa10078ee1dd79d19f7
¨triccyx¨ <triccyx@gmail.com>
2022-09-21 21:42:19 +0200

    fix merge with rr_devel

--- alica_engine/include/engine/AlicaContext.h
+++ alica_engine/include/engine/AlicaContext.h
@@ -497,11 +497,11 @@ void AlicaContext::addSolver(Args&&... args)
 {
     static_assert(std::is_base_of<ISolverBase, SolverType>::value, "Must be derived from ISolverBase");
 #if (defined __cplusplus && __cplusplus >= 201402L)
-    _solvers.emplace(typeid(SolverType).hash_code(),
-            std::make_unique<SolverType>(_engine->editBlackboard(), _engine->getResultStore(), _engine->getConfig(), std::forward<Args>(args)...));
+    _solvers.emplace(typeid(SolverType).hash_code(), std::make_unique<SolverType>(_engine.get()->editBlackboard(), _engine.get()->getResultStore(),
+                                                             _engine.get()->getConfig(), std::forward<Args>(args)...));
 #else
-    _solvers.emplace(typeid(SolverType).hash_code(), std::unique_ptr<SolverType>(new SolverType(_engine->editBlackboard(), _engine->getResultStore(),
-                                                             _engine->getConfig(), std::forward<Args>(args)...)));
+    _solvers.emplace(typeid(SolverType).hash_code(), std::unique_ptr<SolverType>(new SolverType(_engine.get()->editBlackboard(),
+                                                             _engine.get()->getResultStore(), _engine.get()->getConfig(), std::forward<Args>(args)...)));
 #endif
 }
 
