b06700f1f37cced3ef792d285358398970fe4ff0
¨triccyx¨ <triccyx@gmail.com>
2022-10-10 11:47:37 +0200

    added 3 tests

--- supplementary/supplementary_tests/src/test/test_alica_dynamicloadcreator.cpp
+++ supplementary/supplementary_tests/src/test/test_alica_dynamicloadcreator.cpp
@@ -12,7 +12,9 @@
 #include <engine/modelmanagement/factories/PlanFactory.h>
 #include <engine/modelmanagement/factories/RuntimeConditionFactory.h>
 
+#include <cstdlib>
 #include <filesystem>
+#include <stdlib.h>
 
 #include <gtest/gtest.h>
 
@@ -46,7 +48,42 @@ TEST(ForceLoad, simple_behaviour_load)
     if (!std::filesystem::exists(libraryPathFromAlicaYaml)) {
         libraryPathFromAlicaYaml = path + "/../../../../../../devel/lib";
     }
+
     BehaviourContext ctx{&wm, behaviourModel->getName(), behaviourModel, libraryPathFromAlicaYaml, nullptr};
+    std::unique_ptr<BasicBehaviour> behaviour = creator->createBehaviour(10, ctx);
+
+    ASSERT_EQ("acmebehaviour", behaviour->getName());
+}
+
+TEST(ForceLoad, simple_behaviour_withROS_load)
+{
+    ros::NodeHandle nh;
+    std::string path;
+    nh.param<std::string>("/rootPath", path, ".");
+
+    YAML::Node node;
+    try {
+        node = YAML::LoadFile(path + "/etc/plans/behaviours/Acme.beh");
+    } catch (YAML::BadFile& badFile) {
+        std::cerr << path + "/etc/plans/behaviours/Acme.beh" << std::endl;
+        AlicaEngine::abort("MM: Could not parse behaviour file: ", badFile.msg);
+    }
+
+    // Load model
+    Behaviour* behaviourModel;
+    behaviourModel = BehaviourFactory::create(node);
+
+    // Create behaviour form dll
+    IAlicaWorldModel wm;
+    auto creator = std::make_unique<alica::DynamicBehaviourCreator>();
+    std::string rosPackagePath = path + "/../../../../../../install/share/";
+    if (!std::filesystem::exists(rosPackagePath)) {
+        rosPackagePath = path + "/../../../../../../devel/share/";
+    }
+    rosPackagePath = "ROS_PACKAGE_PATH=" + rosPackagePath;
+    char* env = &rosPackagePath[0];
+    putenv(env);
+    BehaviourContext ctx{&wm, behaviourModel->getName(), behaviourModel, "", nullptr};
     std::unique_ptr<BasicBehaviour> behaviour = creator->createBehaviour(10, ctx);
 
     ASSERT_EQ("acmebehaviour", behaviour->getName());
@@ -91,6 +128,48 @@ TEST(ForceLoad, simple_plan_load)
     ASSERT_EQ("acmeplan", plan->getName());
 }
 
+TEST(ForceLoad, simple_plan_withROS_load)
+{
+    ros::NodeHandle nh;
+    std::string path;
+    nh.param<std::string>("/rootPath", path, ".");
+
+    YAML::Node globalNode;
+    try {
+        globalNode = YAML::LoadFile(path + "/etc/hairy/Alica.yaml");
+    } catch (YAML::BadFile& badFile) {
+        std::cerr << path + "/etc/hairy/Alica.yaml" << std::endl;
+        AlicaEngine::abort("MM: Could not parse global config file: ", badFile.msg);
+    }
+
+    YAML::Node node;
+    try {
+        node = YAML::LoadFile(path + "/etc/plans/Acme.pml");
+    } catch (YAML::BadFile& badFile) {
+        std::cerr << path + "/etc/plans/Acme.pml" << std::endl;
+        AlicaEngine::abort("MM: Could not parse plan file: ", badFile.msg);
+    }
+    // Load model
+    ConfigChangeListener configChangeListener(globalNode);
+    Plan* planModel;
+    planModel = PlanFactory::create(configChangeListener, node);
+
+    // Create behaviour form dll
+    IAlicaWorldModel wm;
+    auto creator = std::make_unique<alica::DynamicPlanCreator>();
+    std::string rosPackagePath = path + "/../../../../../../install/share/";
+    if (!std::filesystem::exists(rosPackagePath)) {
+        rosPackagePath = path + "/../../../../../../devel/share/";
+    }
+    rosPackagePath = "ROS_PACKAGE_PATH=" + rosPackagePath;
+    char* env = &rosPackagePath[0];
+    putenv(env);
+    PlanContext ctx{&wm, planModel->getName(), planModel, "", nullptr};
+
+    std::unique_ptr<BasicPlan> plan = creator->createPlan(10, ctx);
+    ASSERT_EQ("acmeplan", plan->getName());
+}
+
 TEST(ForceLoad, simple_condition_load)
 {
     ros::NodeHandle nh;
@@ -127,6 +206,51 @@ TEST(ForceLoad, simple_condition_load)
     }
 
     ConditionContext ctx{conditionModel->getName(), libraryPathFromAlicaYaml, conditionModel->getLibraryName(), 0};
+
+    std::shared_ptr<BasicCondition> condition1 = creator->createConditions(ctx);
+    std::shared_ptr<BasicCondition> condition2 = creator->createConditions(ctx);
+    ASSERT_EQ(true, condition1->evaluate(nullptr, nullptr));
+    ASSERT_EQ(true, condition2->evaluate(nullptr, nullptr));
+}
+
+TEST(ForceLoad, simple_condition_withROS_load)
+{
+    ros::NodeHandle nh;
+    std::string path;
+    nh.param<std::string>("/rootPath", path, ".");
+
+    YAML::Node globalNode;
+    try {
+        globalNode = YAML::LoadFile(path + "/etc/hairy/Alica.yaml");
+    } catch (YAML::BadFile& badFile) {
+        std::cerr << path + "/etc/hairy/Alica.yaml" << std::endl;
+        AlicaEngine::abort("MM: Could not parse global config file: ", badFile.msg);
+    }
+
+    YAML::Node node;
+    try {
+        node = YAML::LoadFile(path + "/etc/plans/conditions/AcmeRuntimeCondition.cnd");
+    } catch (YAML::BadFile& badFile) {
+        std::cerr << path + "/etc/plans/conditions/AcmeRuntimeCondition.cnd" << std::endl;
+        AlicaEngine::abort("MM: Could not parse conditions file: ", badFile.msg);
+    }
+    // Load model
+    ConfigChangeListener configChangeListener(globalNode);
+
+    RuntimeCondition* conditionModel = RuntimeConditionFactory::create(node, nullptr);
+
+    // Create condition form dll
+    IAlicaWorldModel wm;
+    auto creator = std::make_unique<alica::DynamicConditionCreator>();
+    std::string rosPackagePath = path + "/../../../../../../install/share/";
+    if (!std::filesystem::exists(rosPackagePath)) {
+        rosPackagePath = path + "/../../../../../../devel/share/";
+    }
+    rosPackagePath = "ROS_PACKAGE_PATH=" + rosPackagePath;
+    char* env = &rosPackagePath[0];
+    putenv(env);
+
+    ConditionContext ctx{conditionModel->getName(), "", conditionModel->getLibraryName(), 0};
 
     std::shared_ptr<BasicCondition> condition1 = creator->createConditions(ctx);
     std::shared_ptr<BasicCondition> condition2 = creator->createConditions(ctx);
