04847da868d529a122c2036fc4b2e57b0ebf3f4b
dhananjay-patki <114379249+dhananjay-patki@users.noreply.github.com>
2022-10-28 13:37:13 +0900

    Changed entrypoint to better accomodate future testing

--- .github/workflows/ros2_build_test/entrypoint.sh
+++ .github/workflows/ros2_build_test/entrypoint.sh
@@ -3,13 +3,13 @@
 
 colcon build --continue-on-error --packages-skip alica_ros_proxy alica_tracing alica_ros_turtlesim alica_tests supplementary_tests alica_msgs
 
+if [ $? -ne 0 ]
+  then exit 1
+fi 
+
 # colcon test
 
 # colcon test-result --verbose
 
 
-if [ $? -eq 0 ]
-  then exit 0
-fi 
-
-exit 1
+exit 0
