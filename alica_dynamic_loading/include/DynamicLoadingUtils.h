f5bdc94d2d96cc274f7f72f2f676d291727dd60c
Luca Tricerri <triccyx@gmail.com>
2022-11-14 05:25:38 +0100

    Fix path problem on dynamicloading and githubaction (#347)
    Fix pathfinding in test
    
    Co-authored-by: Dale Koenig <dale.koenig@rapyuta-robotics.com>
    Co-authored-by: ¨triccyx¨ <¨triccyx@gmail.com¨>

--- alica_dynamic_loading/include/DynamicLoadingUtils.h
+++ alica_dynamic_loading/include/DynamicLoadingUtils.h
@@ -15,6 +15,5 @@ namespace alica
 std::vector<std::string> tokenizeStr(const std::string& toTokenize, char delimter);
 std::vector<std::string> calculateLibraryPath();
 std::string calculateLibraryCompleteName(const std::vector<std::string>& libraryPath, const std::string& libraryName);
-std::string simplifyPath(const std::string& toSimplify);
 
 } // namespace alica
