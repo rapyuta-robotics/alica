f26dab9e234d5ec552729152a16c636450286d36
Rapyuta Robotics <dhananjay.patki@rapyuta-robotics.com>
2022-12-08 19:23:16 +0900

    Removed ROS dependencies

--- supplementary/alica_ros1/alica_tests/src/test/test_main.cpp
+++ supplementary/alica_ros1/alica_tests/src/test/test_main.cpp
@@ -1,13 +1,10 @@
 #include <gtest/gtest.h>
-#include <ros/ros.h>
 
 int main(int argc, char** argv)
 {
     // std::signal(SIGSEGV, signalHandler);
     // std::signal(SIGABRT, signalHandler);
     testing::InitGoogleTest(&argc, argv);
-    ros::init(argc, argv, "AlicaEngine");
     bool ret = RUN_ALL_TESTS();
-    ros::shutdown();
     return ret;
 }
