53f5d00f431443e101b4b91fc428c19cdf8f0e99
bjoernschroeder <bschroederprogramming@gmail.com>
2022-12-08 21:56:40 +0100

    add missing setTracing implementation to RunnableObject

--- alica_engine/include/engine/RunnableObject.h
+++ alica_engine/include/engine/RunnableObject.h
@@ -86,7 +86,10 @@ protected:
     virtual void doRun() = 0;
     virtual void doTerminate() = 0;
 
-    void setTracing(TracingType type, std::function<std::optional<std::string>()> customTraceContextGetter = {});
+    void setTracing(TracingType type, std::function<std::optional<std::string>()> customTraceContextGetter = {})
+    {
+        _runnableObjectTracer.setTracing(type, customTraceContextGetter);
+    }
     const std::string& getName() { return _name; };
     IAlicaTrace* getTrace() const { return _runnableObjectTracer.getTrace(); };
     // Helper to allow applications to generate their own trace.
