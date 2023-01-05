e8bfaa1c1876ac536c5adb50a46c185d47482a82
¨triccyx¨ <triccyx@gmail.com>
2023-01-05 17:38:23 +0100

    test fix

--- supplementary/alica_ros1/alica_tests/src/test/test_alica_engine_worldmodel.cpp
+++ supplementary/alica_ros1/alica_tests/src/test/test_alica_engine_worldmodel.cpp
@@ -48,8 +48,12 @@ protected:
 
 TEST_F(AlicaTestFixtureWM, simpleGetWM)
 {
-    alicaTests::TestWorldModel* wm = LockedBlackboardRW(ac->editGlobalBlackboard()).get<std::shared_ptr<alicaTests::TestWorldModel>>("worldmodel").get();
-    EXPECT_NE(nullptr, wm);
+    alicaTests::TestWorldModel* wm1 = LockedBlackboardRW(ac->editGlobalBlackboard()).get<std::shared_ptr<alicaTests::TestWorldModel>>("worldmodel").get();
+    EXPECT_NE(nullptr, wm1);
+
+    LockedBlackboardRW(ac->editGlobalBlackboard()).registerValue("worldmodel2", std::make_shared<alicaTests::TestWorldModel>());
+    alicaTests::TestWorldModel* wm2 = LockedBlackboardRW(ac->editGlobalBlackboard()).get<std::shared_ptr<alicaTests::TestWorldModel>>("worldmodel2").get();
+    EXPECT_NE(nullptr, wm2);
 }
 
 } // namespace
