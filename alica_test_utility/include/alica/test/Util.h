6fc3fa891897f688da9f7cb42db6ebf3f9a893be
Veeraj S Khokale <veeraj.khokale@rapyuta-robotics.com>
2023-01-06 16:48:13 +0530

    Address review comments
    - Add STEP_UNTIL_ASSERT_TRUE macro
    - Add comments for the tests
    - Add missing comment for the return value of setTransitionCond

--- alica_test_utility/include/alica/test/Util.h
+++ alica_test_utility/include/alica/test/Util.h
@@ -5,6 +5,7 @@
 #include <engine/BasicPlan.h>
 
 #include <chrono>
+#include <gtest/gtest.h>
 #include <memory>
 
 #define STEP_UNTIL1(condition)                                                                                                                                 \
@@ -44,6 +45,10 @@
         }                                                                                                                                                      \
         std::this_thread::sleep_for(std::chrono::milliseconds(10));                                                                                            \
     } while (0)
+
+#define STEP_UNTIL_ASSERT_TRUE(ac, condition)                                                                                                                  \
+    STEP_UNTIL(ac, condition);                                                                                                                                 \
+    ASSERT_TRUE(condition)
 
 #define SLEEP_UNTIL(condition)                                                                                                                                 \
     do {                                                                                                                                                       \
