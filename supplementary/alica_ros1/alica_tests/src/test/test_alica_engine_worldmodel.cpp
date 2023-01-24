b90eb5032e3bc3a6b0f3d632f44acde3d7539a1e
¨triccyx¨ <triccyx@gmail.com>
2023-01-24 13:46:10 +0100

    fix plan design error

--- supplementary/alica_ros1/alica_tests/src/test/test_alica_engine_worldmodel.cpp
+++ supplementary/alica_ros1/alica_tests/src/test/test_alica_engine_worldmodel.cpp
@@ -5,32 +5,6 @@
 #include <alica_tests/TestFixture.h>
 #include <gtest/gtest.h>
 
-/*
-TODO Luca
-uncomment when it will be possible to add transition TestMasterPlan->ConstraintTestPlan
-namespace alica::test
-{
-
-TEST_F(TestFixture, simpleGetWM)
-{
-    // Transition to the plan corresponding to this test case
-    ASSERT_TRUE(_tc->setTransitionCond("TestMasterPlan", "ChooseTestState", "ConstraintTestPlanState")) << _tc->getLastFailure();
-    STEP_UNTIL(_tc, _tc->getActivePlan("ConstraintTestPlan"));
-    ASSERT_NE(nullptr, _tc->getActivePlan("ConstraintTestPlan")) << _tc->getLastFailure();
-
-    LockedBlackboardRW(_tc->editGlobalBlackboard()).set("worldmodel1", std::make_shared<alicaTests::TestWorldModelNew>(_tc.get()));
-    std::shared_ptr<alicaTests::TestWorldModelNew> wm1 =
-            LockedBlackboardRO(_tc->editGlobalBlackboard()).get<std::shared_ptr<alicaTests::TestWorldModelNew>>("worldmodel1");
-    EXPECT_NE(nullptr, wm1);
-
-    LockedBlackboardRW(_tc->editGlobalBlackboard()).set("worldmodel2", std::make_shared<alicaTests::TestWorldModelNew>(_tc.get()));
-    std::shared_ptr<alicaTests::TestWorldModelNew> wm2 =
-            LockedBlackboardRO(_tc->editGlobalBlackboard()).get<std::shared_ptr<alicaTests::TestWorldModelNew>>("worldmodel2");
-    EXPECT_NE(nullptr, wm2);
-}
-} // namespace alica::test
-*/
-
 namespace alica
 {
 namespace
