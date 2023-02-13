ad00732ec7960f9cbb378b4653769f9e3fe5272a
Dale Koenig <dale.koenig@rapyuta-robotics.com>
2023-02-13 15:32:37 +0900

    Review round

--- alica_engine/src/engine/modelmanagement/factories/BlackboardBlueprintFactory.cpp
+++ alica_engine/src/engine/modelmanagement/factories/BlackboardBlueprintFactory.cpp
@@ -12,8 +12,7 @@ std::unique_ptr<BlackboardBlueprint> BlackboardBlueprintFactory::create(const YA
     for (const auto& entry : node) {
         auto key = getValue<std::string>(entry, Strings::key);
         auto type = getValue<std::string>(entry, Strings::stateType);
-        std::optional<std::string> defaultValue; // deprecated
-        blueprint->addKey(key, type, defaultValue);
+        blueprint->addKey(key, type);
     }
     return blueprint;
 }
