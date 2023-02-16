4db8e6be05af3b26cb2f007df9702ff28ec4a96e
Dale Koenig <dale.koenig@rapyuta-robotics.com>
2023-02-16 18:24:44 +0900

    Add alica_standard_library package to use for common conditions (#411)

--- alica_standard_library/include/Conditions.h
+++ alica_standard_library/include/Conditions.h
@@ -9,8 +9,12 @@ namespace utils
 
 bool AnyChildSuccess(const alica::Blackboard* input, const alica::RunningPlan* rp, const alica::Blackboard* globalBlackboard);
 bool AllChildSuccess(const alica::Blackboard* input, const alica::RunningPlan* rp, const alica::Blackboard* globalBlackboard);
+bool AnyChildFailure(const alica::Blackboard* input, const alica::RunningPlan* rp, const alica::Blackboard* globalBlackboard);
+bool AllChildFailure(const alica::Blackboard* input, const alica::RunningPlan* rp, const alica::Blackboard* globalBlackboard);
 
 BOOST_DLL_ALIAS(utils::AnyChildSuccess, AnyChildSuccess)
 BOOST_DLL_ALIAS(utils::AllChildSuccess, AllChildSuccess)
+BOOST_DLL_ALIAS(utils::AnyChildFailure, AnyChildFailure)
+BOOST_DLL_ALIAS(utils::AllChildFailure, AllChildFailure)
 
 } /* namespace utils */
