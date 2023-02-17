3fa9bc4fecc73f5cce0bcdc05dafc19e13e84b4a
Maksim Derbasov <ntfs.hard@gmail.com>
2023-02-17 22:17:38 +0900

    Cleanup

--- supplementary/alica_ros1/alica_ros_proxy/include/logger/AlicaRosLogger.h
+++ supplementary/alica_ros1/alica_ros_proxy/include/logger/AlicaRosLogger.h
@@ -16,12 +16,11 @@ namespace alicaRosLogger
 class AlicaRosLogger : public alica::IAlicaLogger
 {
 public:
-    AlicaRosLogger(const Verbosity verbosity, const std::string& localAgentName, const alica::AgentId localAgentId);
+    AlicaRosLogger(const Verbosity verbosity, const std::string& localAgentName);
     void log(const std::string& msg, const Verbosity verbosity, const std::string& logSpace) override;
 
 private:
     const std::string _localAgentName;
-    const alica::AgentId _localAgentId;
     static const std::unordered_map<alica::Verbosity, ros::console::Level> _verbosityRosLevelMap;
 };
 
