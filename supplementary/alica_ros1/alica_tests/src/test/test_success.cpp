5f005882893800404ed6efddfb4807aae6c19389
Maksim Derbasov <ntfs.hard@gmail.com>
2023-02-20 15:24:10 +0900

    Cleanup (#426)

--- supplementary/alica_ros1/alica_tests/src/test/test_success.cpp
+++ supplementary/alica_ros1/alica_tests/src/test/test_success.cpp
@@ -31,7 +31,7 @@ public:
         _spinner = std::make_unique<ros::AsyncSpinner>(config["Alica"]["ThreadPoolSize"].as<int>(4));
         _tc->setCommunicator<alicaDummyProxy::AlicaDummyCommunication>();
         _tc->setTimerFactory<alicaRosTimer::AlicaRosTimerFactory>();
-        _tc->setLogger<alicaRosLogger::AlicaRosLogger>(config["Local"]["ID"].as<int>());
+        _tc->setLogger<alicaRosLogger::AlicaRosLogger>();
         LockedBlackboardRW(_tc->editGlobalBlackboard()).set("worldmodel", std::make_shared<alicaTests::TestWorldModelNew>(_tc.get()));
         AlicaCreators creators{std::make_unique<alica::ConditionCreator>(), std::make_unique<alica::UtilityFunctionCreator>(),
                 std::make_unique<alica::ConstraintCreator>(), std::make_unique<alica::DynamicBehaviourCreator>(), std::make_unique<alica::PlanCreator>(),
