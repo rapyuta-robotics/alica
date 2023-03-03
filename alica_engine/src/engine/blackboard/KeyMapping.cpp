f172ef38e926480a9c4924008e96bf91af806f57
Veeraj S Khokale <veeraj.khokale@rapyuta-robotics.com>
2023-03-03 17:19:27 +0530

    Add mapping APIs

--- alica_engine/src/engine/blackboard/KeyMapping.cpp
+++ alica_engine/src/engine/blackboard/KeyMapping.cpp
@@ -14,6 +14,11 @@ const KeyMapping::KeyMappingList& KeyMapping::getInputMapping() const
     return _inputMapping;
 }
 
+const KeyMapping::KeyMappingList& KeyMapping::getInputValueMapping() const
+{
+    return _inputValueMapping;
+}
+
 const KeyMapping::KeyMappingList& KeyMapping::getOutputMapping() const
 {
     return _outputMapping;
@@ -21,14 +26,32 @@ const KeyMapping::KeyMappingList& KeyMapping::getOutputMapping() const
 
 void KeyMapping::addInputMapping(const std::string& parentKey, const std::string& childKey)
 {
-    assert(std::count(_inputMapping.begin(), _inputMapping.end(), std::make_pair(parentKey, childKey)) == 0);
-    _inputMapping.push_back(std::make_pair(parentKey, childKey));
+    if (std::count_if(_inputMapping.begin(), _inputMapping.end(), [childKey](const auto& mapping) { return mapping.target == childKey; })) {
+        throw std::logic_error("key: " + childKey + " already has an input mapping");
+    }
+    if (std::count_if(_inputValueMapping.begin(), _inputValueMapping.end(), [childKey](const auto& mapping) { return mapping.target == childKey; })) {
+        throw std::logic_error("key: " + childKey + " already has an input value mapping");
+    }
+    _inputMapping.emplace_back(parentKey, childKey);
+}
+
+void KeyMapping::addInputValueMapping(const std::string& value, const std::string& childKey)
+{
+    if (std::count_if(_inputMapping.begin(), _inputMapping.end(), [childKey](const auto& mapping) { return mapping.target == childKey; })) {
+        throw std::logic_error("key: " + childKey + " already has an input mapping");
+    }
+    if (std::count_if(_inputValueMapping.begin(), _inputValueMapping.end(), [childKey](const auto& mapping) { return mapping.target == childKey; })) {
+        throw std::logic_error("key: " + childKey + " already has an input value mapping");
+    }
+    _inputValueMapping.emplace_back(value, childKey);
 }
 
 void KeyMapping::addOutputMapping(const std::string& parentKey, const std::string& childKey)
 {
-    assert(std::count(_outputMapping.begin(), _outputMapping.end(), std::make_pair(parentKey, childKey)) == 0);
-    _outputMapping.push_back(std::make_pair(parentKey, childKey));
+    if (std::count_if(_outputMapping.begin(), _outputMapping.end(), [parentKey](const auto& mapping) { return mapping.target == parentKey; })) {
+        throw std::logic_error("key: " + parentKey + " already has an output mapping");
+    }
+    _outputMapping.emplace_back(childKey, parentKey);
 }
 
 } /* namespace alica */
