f6b69fa02c6e75493dba135273ad44cb94e4f696
Veeraj S Khokale <veeraj.khokale@rapyuta-robotics.com>
2023-03-24 23:22:36 +0530

    Small fix

--- alica_dynamic_loading/src/DynamicConstraintCreator.cpp
+++ alica_dynamic_loading/src/DynamicConstraintCreator.cpp
@@ -18,20 +18,21 @@ DynamicConstraintCreator::DynamicConstraintCreator()
 std::shared_ptr<BasicConstraint> DynamicConstraintCreator::createConstraint(int64_t constraintConfId, ConstraintContext& context)
 {
     std::string completeLibraryName;
+    std::string symbolName = context.name + "Constraint"; // symbol to import, append `Constraint` to name because the name is the same as the condition's name
     try {
         completeLibraryName = calculateLibraryCompleteName(_libraryPath, context.libraryName);
     } catch (const DynamicLoadingException& ex) {
-        throw DynamicLoadingException{"constraint", constraintConfId, context.name, "", context.libraryName, ex.what()};
+        throw DynamicLoadingException{"constraint", constraintConfId, symbolName, "", context.libraryName, ex.what()};
     }
 
     try {
         _constraintCreator = boost::dll::import_alias<constraintCreatorType>( // type of imported symbol must be explicitly specified
                 completeLibraryName,                                          // complete path to library also with file name
-                context.name + "Constraint",              // symbol to import, append `Constraint` to name because the name is the same as the condition's name
+                symbolName,
                 boost::dll::load_mode::append_decorations // do append extensions and prefixes
         );
     } catch (const std::exception& ex) {
-        throw DynamicLoadingException{"constraint", constraintConfId, context.name, "", context.libraryName, ex.what()};
+        throw DynamicLoadingException{"constraint", constraintConfId, symbolName, "", context.libraryName, ex.what()};
     }
 
     std::shared_ptr<BasicConstraint> createdConstraint = _constraintCreator(context);
