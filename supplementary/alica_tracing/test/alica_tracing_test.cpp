71fdc6904376dee7746bf9552b56714c62f2b53b
Rogerio Fonteles <rogerio.fonteles@moley.com>
2023-05-04 05:03:08 +0000

    pre-commit

--- supplementary/alica_tracing/test/alica_tracing_test.cpp
+++ supplementary/alica_tracing/test/alica_tracing_test.cpp
@@ -1,11 +1,12 @@
-#include <tracing/TraceFactory.h>
 #include <tracing/Trace.h>
+#include <tracing/TraceFactory.h>
 
 #include <gtest/gtest.h>
 
 TEST(TestAmrTracing, TestAmrTracingInit)
 {
-    auto tf = alicaTracing::TraceFactory("amr_routing", "/home/rr/catkin_ws/src/rr_io_amr/rr_di_core/robot/external/alica/supplementary/alica_tracing/config/tracer_default.config");
+    auto tf = alicaTracing::TraceFactory(
+            "amr_routing", "/home/rr/catkin_ws/src/rr_io_amr/rr_di_core/robot/external/alica/supplementary/alica_tracing/config/tracer_default.config");
     // Report init
     auto trace = tf.create("init", std::nullopt);
     trace->setTag("Init", "initialized");
