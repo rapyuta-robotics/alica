cd3468d94ccddc876047cfaede2a991bd98d4c73
bjoernschroeder <bschroederprogramming@gmail.com>
2023-07-12 09:59:14 +0200

    remove ros2 changes

---
+++ supplementary/alica_ros2/alica_ros2_turtlesim/libalica-ros2-turtlesim/src/Master.cpp
@@ -0,0 +1,38 @@
+#include "Master.h"
+#include "world_model.hpp"
+
+namespace turtlesim
+{
+
+Master::Master(alica::PlanContext& context)
+        : alica::BasicPlan(context)
+{
+}
+Master::~Master() {}
+std::unique_ptr<Master> Master::create(alica::PlanContext& context)
+{
+    return std::make_unique<Master>(context);
+}
+
+void Master::onInit()
+{
+    ALICATurtleWorldModel::init();
+}
+
+void Master::onTerminate()
+{
+    ALICATurtleWorldModel::del();
+}
+
+std::shared_ptr<alica::UtilityFunction> MasterUtilityFunction::getUtilityFunction(alica::Plan* plan)
+{
+    std::shared_ptr<alica::UtilityFunction> defaultFunction = std::make_shared<alica::DefaultUtilityFunction>(plan);
+    return defaultFunction;
+}
+
+std::shared_ptr<MasterUtilityFunction> MasterUtilityFunction::create(alica::UtilityFunctionContext&)
+{
+    return std::make_shared<MasterUtilityFunction>();
+}
+
+} // namespace turtlesim
