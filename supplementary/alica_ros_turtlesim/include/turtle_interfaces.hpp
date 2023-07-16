3f35b719ca5e4f08af906fc0d81eea0f1645e5b6
bjoernschroeder <bschroederprogramming@gmail.com>
2023-07-16 17:44:57 +0200

    remove old code

--- supplementary/alica_ros_turtlesim/include/turtle_interfaces.hpp
+++ supplementary/alica_ros_turtlesim/include/turtle_interfaces.hpp
@@ -24,18 +24,9 @@ public:
     virtual bool teleport(const float x, const float y) = 0;                 // teleport turtle to (x,y)
     virtual bool spawn() = 0;                                                // Spawn the turtle in the middle of the map
     virtual bool moveTowardPosition(const float x, const float y) const = 0; // publish cmd_vel based on input(x,y) and current pose
-    // PoseConstPtr getCurrentPose() const { return _currentPose; }; // Retrieve current pose if available
     virtual void rotate(const float dYaw) = 0;
-    virtual void subOnMsg(const std::string& topic, alica::Blackboard* globalBlackboard) = 0;
-    virtual void subOnTrigger(const std::string& topic, alica::Blackboard* globalBlackboard) = 0;
 
 protected:
-    // void poseSubCallback(const PoseConstPtr& msg); // callback of /pose from the turtlesim
-    // ros::Publisher _velPub;                        // publish cmd_vel to the turtlesim
-    // ros::Subscriber _poseSub;                      // subscribe turtleX/pose from the turtlesim
-    // ros::ServiceClient _teleportClient;            // client of teleportAbsolute service
-    // ros::ServiceClient _spawnClient;
-    // PoseConstPtr _currentPose; // current position
     std::string _name;
 };
 
