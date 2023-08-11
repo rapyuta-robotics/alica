5bf487f9f27980fb5b69bdd17ef50e10d6ff48d5
bjoernschroeder <bjoernschroder@rapyuta-robotics.com>
2023-08-11 09:28:32 +0200

    fix includes

--- supplementary/alica_ros_turtlesim/libalica-turtlesim/include/MakeFormation.h
+++ supplementary/alica_ros_turtlesim/libalica-turtlesim/include/MakeFormation.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "engine/BasicPlan.h"
-#include "engine/BasicUtilityFunction.h"
 #include <boost/dll/alias.hpp>
+#include <engine/BasicPlan.h>
+#include <engine/BasicUtilityFunction.h>
 
 namespace turtlesim
 {
